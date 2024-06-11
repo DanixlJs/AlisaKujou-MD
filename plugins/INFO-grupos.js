4c2842ec113c973cd6ca3b39ced117612f1ccb19
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-11 15:11:45 -0400

    Actualizar INFO-grupos.js

--- plugins/INFO-grupos.js
+++ plugins/INFO-grupos.js
@@ -2,10 +2,14 @@ let media = global.icons
 let handler = async (m, { conn, command }) => {
 
 let str = `❀ *GRUPOS OFICIALES*
-✰ *⏤͟͟͞͞𝐄𝐥 𝐒𝐨́𝐭𝐚𝐧𝐨 𝐝𝐞 𝐀𝐥𝐢𝐬𝐚 🤍*
+✰ 🤍𝗔𝗹𝗶𝘀𝗮 𝗞𝘂𝗷𝗼𝘂​᭄ 𝚡 ⏤͟͟͞͞𝐍𝐞𝐤𝐨'𝐬 𝐂𝐥𝐮𝐛⏤͟͟͞͞★
 > [https://chat.whatsapp.com/Kbj38zCqOvqH9KM5bRH1Hb]
 ✰ *🤍 𝗔𝗹𝗶𝘀𝗮 𝗞𝘂𝗷𝗼𝘂 - 𝗠𝗗​᭄ ᎪΝႮΝᏟᏆϴՏ*
-> [https://whatsapp.com/channel/0029Vaa4kxh4dTnEJ0pwVr1y]`
+> [https://whatsapp.com/channel/0029Vaa4kxh4dTnEJ0pwVr1y]
+
+❀ *GRUPOS DE COLABORACIÓN*
+✰ *🌸𝒀𝒐𝒔𝒉𝒊𝒌𝒐𝑩𝒐𝒕-𝑴𝑫 x ☙𝘼𝙡𝙞𝙨𝙖 𝙆𝙪𝙟𝙤𝙪​᭄*
+> [https://chat.whatsapp.com/GvJAupaHXTQDNivZIa2l5D]`
 
 m.react('🧰') 
 
@@ -15,6 +19,5 @@ handler.command = ['grupos']
 handler.help = ['grupos']
 handler.tags = ['info']
 handler.registrado = true
-handler.exp = 33
 
 export default handler
