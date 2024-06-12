1f364c4fdce6a506040cb2941c822a4252b047f4
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-12 11:20:56 -0400

    Update SEARCH-pinterest.js

--- plugins/SEARCH-pinterest.js
+++ plugins/SEARCH-pinterest.js
@@ -1,7 +1,7 @@
 import Scraper from "@SumiFX/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply('✧ Ingresa un texto para realizar la búsqueda.`)
+if (!text) return m.reply('✧ Ingresa un texto para realizar la búsqueda.')
 try {
 let { dl_url } = await Scraper.pinterest(text)
 await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', null, m)
