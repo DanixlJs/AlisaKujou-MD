a05ecb6ebaf843c01eb5cc70cba5768e706c3d5b
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-12 04:57:27 -0400

    Update INFO-reporte.js

--- plugins/INFO-reporte.js
+++ plugins/INFO-reporte.js
@@ -31,8 +31,8 @@ export default handler;
 function msToTime(duration) {
   const milliseconds = parseInt((duration % 1000) / 100);
   let seconds = Math.floor((duration / 1000) % 60);
-  let minutes = Math.floor((duration / (1000 * 60)) * 60);
-  let hours = Math.floor((duration / (1000 * 60 * 60)) * 24);
+  let minutes = Math.floor((duration / (1000 * 60)) % 60);
+  let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
   hours = (hours < 10) ? '0' + hours : hours;
   minutes = (minutes < 10) ? '0' + minutes : minutes;
   seconds = (seconds < 10) ? '0' + seconds : seconds;
