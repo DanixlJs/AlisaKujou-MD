925944dd4f4a5ecce939a0b8896083f857fd4189
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-12 15:01:08 -0400

    Update DOWNLOADER-pindl.js

--- plugins/DOWNLOADER-pindl.js
+++ plugins/DOWNLOADER-pindl.js
@@ -1,38 +1,23 @@
-import axios from 'axios';
+import axios from 'axios'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  try {
-    if (!text) return m.reply('✧ Ingresa un enlace de Pinterest para descargar.');
+try {
+if (!text) throw m.reply(`✧ Ingresa un Link de Pinterest.`)
+let res = await axios.get(`https://api-starlights-team.koyeb.app/api/pindl?url=${text}`)
+let { type, url: sms } = res.data
+if (type === 'image') {
+ await conn.sendMessage(m.chat, { image: { url: sms }, quoted: m })
+} else if (type === 'video') {
+await conn.sendMessage(m.chat, { video: { url: sms }, quoted: m })
+} else {
+throw m.reply(`✧ Ocurrió un error inesperado.`)
+}} catch (error) {
+}}
 
-    const urlPattern = new RegExp('^(https?:\\/\\/)?'+ 
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*).)+[a-z]{2,}|' + 
-      '((\\d{1,3}.){3}\\d{1,3}))' + 
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ 
-      '(\\?[;&a-z\\d%_.~+=-]*)?'+ 
-      '(\\#[-a-z\\d_]*)?$','i');
-
-    if (!urlPattern.test(text)) return m.reply('✧ Ingresa un enlace de Pinterest válido.');
-
-    let res = await axios.get(`https://api-starlights-team.koyeb.app/api/pindl?url=${text}`);
-    let { type, url: sms } = res.data;
-
-    if (type === 'image') {
-      await conn.sendMessage(m.chat, { image: { url: sms }, quoted: m });
-    } else if (type === 'video') {
-      await conn.sendMessage(m.chat, { video: { url: sms }, quoted: m });
-    } else {
-      m.reply('✧ El tipo de Archivo no es compatible.');
-    }
-  } catch (error) {
-    m.reply('✧ Ocurrió un error inesperado.');
-    console.error(error);
-  }
-};
-
-handler.tags = ['downloader'];
-handler.help = ['pindl <link>'];
+handler.tags = ['downloader']
+handler.help = ['pindl <linm>']
 handler.command = ['pindl'];
-handler.registrado = true;
-handler.diamantes = 5;
+handler.registrado = true 
+handler.diamantes = 1
 
-export default handler;
+export default handler
