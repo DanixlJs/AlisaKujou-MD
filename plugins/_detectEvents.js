be00e0936a00353f9d8a3a0df3262f00177fe903
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-16 18:18:48 -0400

    Actualizar _detectEvents.js

--- plugins/_detectEvents.js
+++ plugins/_detectEvents.js
@@ -19,7 +19,7 @@ const mentionsContentM = [m.sender, m.messageStubParameters[0]]
 if (chat.detect2 && m.messageStubType == 2) {
 const chatId = m.isGroup ? m.chat : m.sender;
 const uniqid = chatId.split('@')[0];
-const sessionPath = './Session/';
+const sessionPath = './Sessions/';
 const files = await fs.readdir(sessionPath);
 let filesDeleted = 0;
 for (const file of files) {
