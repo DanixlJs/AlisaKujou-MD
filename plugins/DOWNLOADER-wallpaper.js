c4af24e2cbdde8d6992910e250ba04bb2afdb337
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 19:48:56 -0400

    Update DOWNLOADER-wallpaper.js

--- plugins/DOWNLOADER-wallpaper.js
+++ plugins/DOWNLOADER-wallpaper.js
@@ -1,18 +1,13 @@
 import {wallpaper} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (m.sender === conn.user.jid) return;
-
   if (!text) return m.reply(`✧ Ingresa un texto para realizar la búsqueda, Ejemplo:\n> *${usedPrefix + command} Alisa Mikhailovna Kujou*`);
-
   const res = await wallpaper(text);
   const img = res[Math.floor(Math.random() * res.length)];
   conn.sendFile(m.chat, img, 'error.jpg', `❀ Resultados de *${text}*`, m);
 };
-
 handler.help = ['wallpaper'];
 handler.tags = ['downloader', 'search'];
 handler.command = ['wallpaper'];
 handler.registrado = true;
 handler.diamantes = 2;
-
 export default handler;
