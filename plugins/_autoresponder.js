11275c8818af7a0a1f0e14bf3a265d762d5f7647
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 15:09:52 -0400

    Actualizar _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -1,10 +1,7 @@
 import { sticker } from '../lib/sticker.js'
 let handler = m => m
-
 handler.all = async function (m, {conn}) {
-if (this.user.jid) return;
 let chat = global.db.data.chats[m.chat]
-
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
     if (areJidsSameUser(m.sender, this.user.id)) {
     return;
@@ -12,6 +9,5 @@ if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(img2, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
 contextInfo: { externalAdReply: { title: global.wm, body: global.dev, sourceUrl: fakeLink, thumbnail: global.icons}}})}
-
 return !0 }
 export default handler
