cd72011c26d067339c4346ade0881c7f25b804ea
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 19:21:17 -0400

    Update TOOLS-toimg.js

--- plugins/TOOLS-toimg.js
+++ plugins/TOOLS-toimg.js
@@ -1,8 +1,5 @@
 import { webp2png } from '../lib/webp2mp4.js'
-
 var handler = async (m, { conn, usedPrefix, command }) => {
-if ( m.sender === conn.user.jid) return
-
 const notStickerMessage = `✧ Responde a un Sticker.`
 if (!m.quoted) return m.reply(notStickerMessage)
 const q = m.quoted || m
@@ -11,12 +8,9 @@ if (!/sticker/.test(mime)) return m.reply(notStickerMessage)
 let media = await q.download()
 let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
 await conn.sendFile(m.chat, out, 'error.png', null, m, m)
-
 }
-
 handler.help = ['toimg']
 handler.tags = ['tools']
 handler.command = ['toimg']
 handler.registrado = true
-
 export default handler
