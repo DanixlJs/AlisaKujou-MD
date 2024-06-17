f9d39a057b271580849bf4a369db68f1eaec101a
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 09:55:37 -0400

    Update INFO-ping.js

--- plugins/INFO-ping.js
+++ plugins/INFO-ping.js
@@ -2,7 +2,6 @@ import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
-if (m.sender === conn.user.jid) return
          let timestamp = speed();
          let latensi = speed() - timestamp;
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
