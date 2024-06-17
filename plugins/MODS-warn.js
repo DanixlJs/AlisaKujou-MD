60fe7b8ff405a50f3601430e3afa45106b70df9d
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 11:00:25 -0400

    Update MODS-warn.js

--- plugins/MODS-warn.js
+++ plugins/MODS-warn.js
@@ -1,9 +1,7 @@
 const handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (m.sender === conn.user.jid) return;
     let user;
     let reason;
     let db = global.db.data.users
-
     if (m.quoted) {
         user = m.quoted.sender;
         reason = args.join(' ');
@@ -14,7 +12,6 @@ if (m.sender === conn.user.jid) return;
         await conn.reply(m.chat, `✧ Etiqueta o responde al mensaje del usuario que quieras Advertir, Ejemplo:\n> → *${usedPrefix}warn <@tag> <razón>*\n> *${usedPrefix}warn <razón>*`, m);
         return;
     }
-
     if (db[user]) {
         if (!db[user].warn) {
             db[user].warn = 0;
@@ -24,22 +21,17 @@ if (m.sender === conn.user.jid) return;
         }
         db[user].warn += 1;
         db[user].warnRazon = `${reason}`;
-
         const nametag = await conn.getName(user);
-
         await conn.reply(m.chat, `❀ El usuario *${nametag}* ha sido advertido.\n> ✰ *Advertencias ⪼* ${db[user].warn}\n> ◈ *Razón ⪼* ${reason}`, m, fake, { mentionedJid: [user] });
        let nn = conn.getName(m.sender);
-
        conn.reply('120363284046748076@g.us', `✧ El usuario *${nametag}* recibió una advertencia de *${nn}*.`, m, fake, { mentionedJid: [user] });
     } else {
         await conn.reply(m.chat, `El usuario no está registrado.`, m);
     }
 };
-
 handler.help = ['warn <@tag> <razón>'];
 handler.command = ['warn'];
 handler.tags = ['mods'];
 handler.mods = true;
 handler.group = true;
-
 export default handler;
