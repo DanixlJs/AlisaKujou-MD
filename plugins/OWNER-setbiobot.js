8c7a5307c169d1d628838d53aa04cea13581e4b9
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 12:51:58 -0400

    Oni-chan

--- plugins/OWNER-setbiobot.js
+++ plugins/OWNER-setbiobot.js
@@ -1,18 +1,15 @@
 let handler = async (m, { conn, text }) => {
-if ( m.sender === conn.user.jid) return
    if (!text) return m.reply(`✧ Ingresa la nueva biografía del Bot.`)
      try {
                 await conn.updateProfileStatus(text).catch(_ => _)
                 conn.reply(m.chat, `❀ Biografía actualizada con éxito.`, m)
 } catch {
-       throw '✧ Ocurrió un error inesperado.'
+       m.reply('✧ Ocurrió un error inesperado.')
      }
 }
-
 handler.help = ['setbotbio <texto>']
 handler.tags = ['owner']
 handler.command = ['setbiobot']
 handler.owner = true
 handler.registrado = true
-
 export default handler
