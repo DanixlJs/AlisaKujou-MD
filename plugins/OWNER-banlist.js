4ba39642f216eedf169f3cc3537184274d0fed59
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 12:17:04 -0400

    UwU

--- plugins/OWNER-banlist.js
+++ plugins/OWNER-banlist.js
@@ -1,26 +1,17 @@
 const handler = async (m, {conn, isOwner}) => {
-if (m.sender === conn.user.jid) return
-
   const chats = Object.entries(global.db.data.chats).filter((chat) => chat[1].isBanned);
-
   const users = Object.entries(global.db.data.users).filter((user) => user[1].baneado);
-
   const caption = `❀ *USUARIOS BANEADOS*
 ✰ *Total ⪼* ${users.length} ${users ? '\n' + users.map(([jid], i) => `
 ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : ''}
-
 ❀ *CHATS MUTEADOS*
 ✰ *Total ⪼* ${chats.length} ${chats ? '\n' + chats.map(([jid], i) => `
 ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : ''}
 `.trim();
-
   m.reply(caption, null, {mentions: conn.parseMention(caption)});
 };
-
 handler.help = ['banlist'];
 handler.command = ['banlist'];
 handler.rowner = true;
 handler.registrado = true;
-
-
 export default handler;
