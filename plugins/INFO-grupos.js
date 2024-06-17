da7cc83ec3fc9605c85cacd1cf4efe79666d2244
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 08:27:59 -0400

    Update INFO-grupos.js

--- plugins/INFO-grupos.js
+++ plugins/INFO-grupos.js
@@ -1,17 +1,19 @@
-let media = global.icons
 let handler = async (m, { conn, command }) => {
-if (m.sender === conn.user.jid) return
-
 let str = `❀ *GRUPOS OFICIALES*
+
+❀ *GRUPOS DE COLABORACIÓN*
 ✰ 🤍𝗔𝗹𝗶𝘀𝗮 𝗞𝘂𝗷𝗼𝘂​᭄ 𝚡 ⏤͟͟͞͞𝐍𝐞𝐤𝐨'𝐬 𝐂𝐥𝐮𝐛⏤͟͟͞͞★
 > [https://chat.whatsapp.com/Kbj38zCqOvqH9KM5bRH1Hb]
+
 ✰ *🤍 𝗔𝗹𝗶𝘀𝗮 𝗞𝘂𝗷𝗼𝘂 - 𝗠𝗗​᭄ ᎪΝႮΝᏟᏆϴՏ*
 > [https://whatsapp.com/channel/0029Vaa4kxh4dTnEJ0pwVr1y]
 
-❀ *GRUPOS DE COLABORACIÓN*
+❀ *CANALES OFICIALES*
+✰ *🤍 𝗔𝗹𝗶𝘀𝗮 𝗞𝘂𝗷𝗼𝘂 - 𝗠𝗗​᭄ ᎪΝႮΝᏟᏆϴՏ*
+> [https://whatsapp.com/channel/0029Vaa4kxh4dTnEJ0pwVr1y]
 `
-
-await conn.reply(m.chat, str, m, fake, )}
+await conn.reply(m.chat, str, m, fake, )
+}
 
 handler.command = ['grupos']
 handler.help = ['grupos']
