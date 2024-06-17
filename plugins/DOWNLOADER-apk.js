525ea54636da907c3f22836ef1a99a9011f43316
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 14:14:35 -0400

    Actualizar DOWNLOADER-apk.js

--- plugins/DOWNLOADER-apk.js
+++ plugins/DOWNLOADER-apk.js
@@ -1,29 +1,17 @@
-import { search, download } from 'aptoide-scraper'
-
-var handler = async (m, {conn, usedPrefix, command, text}) => {
-
-if (!text) return conn.reply(m.chat, `✧ Ingresa el nombre del Apk que quieras descargar, Ejemplo\n> *${usedPrefix + command} CapCut*`, m, )
+import Scraper from "@SumiFX/Scraper"
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return m.reply('✧ Ingresa el nombre del APK qué quieras descargar.')
 try {
-let searchA = await search(text)
-let data5 = await download(searchA[0].id)
-
-let response = `❀ *APTOIDE DL*\n✰ *Nombre:* ${data.name}\n◈ *Paquete:* ${data5.package}\n◈ *Actualización:* ${data5.lastup}\n◈ *Tamaño:* ${data5.size}`
-
-await conn.sendMessage(m.chat, { text: response, contextInfo: { externalAdReply: { title: data5.name, body: global.wm, sourceUrl: global.fakeLink, thumbnailUrl: data5.icon, mediaType: 1, showAdAttribution: true, renderLargerThumbnail: true }}} , { quoted: m })   
-
- if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-return await conn.reply(m.chat, '✧ El archivo es demasiado pesado.', m, )
-}
-await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m})
+let { name, packname, update, size, thumbnail, dl_url } = await Scraper.aptoide(text)
+if (size.includes('GB') || size.replace(' MB', '') > 200) { return await m.reply('✧ El archivo pesa mas de 200 MB, se canceló la Descarga.')}
+let txt = `❀ *APTOIDE DL*\n✰ *Nombre ⪼* ${name}\n◈ *Packname ⪼* ${packname}\n◈ *Peso ⪼* ${size}\n◈ *Actualizado el ⪼* ${update}`
+await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m)
+await conn.sendMessage(m.chat, {document: { url: dl_url }, mimetype: 'application/vnd.android.package-archive', fileName: name + '.apk', caption: null }, {quoted: m})
 } catch {
-await m.reply(m.chat, '✧ Ocurrió un error inesperado..')
-}    
-}
-
+}}
+handler.help = ['apk <aplicación>']
 handler.tags = ['downloader']
-handler.help = ['apkmod <texto>']
-handler.command = ['apkmod']
-handler.registrado = true
-handler.diamantes = 10
-
+handler.command = ['aptoide', 'apk']
+handler.registrado = true 
+handler.diamantes = 5
 export default handler
