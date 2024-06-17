5794141989442c0a008181c495e475de546f84cb
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 10:33:05 -0400

    Update MODS-ban.js

--- plugins/MODS-ban.js
+++ plugins/MODS-ban.js
@@ -1,9 +1,7 @@
-const handler = async (m, { conn, args, text, usedPrefix, command, isOwner }) => {
-    if (m.sender === conn.user.jid) return
+const handler = async (m, { conn, args, text, usedPrefix, command, isROwner }) => {
     let user;
     let reason;
     let db = global.db.data.users;
-
     if (m.quoted) {
         user = m.quoted.sender;
         reason = args.join(' ');
@@ -11,22 +9,19 @@ const handler = async (m, { conn, args, text, usedPrefix, command, isOwner }) =>
         user = args[0].replace('@', '') + '@s.whatsapp.net';
         reason = args.slice(1).join(' ');
     } else {
-        await conn.reply(m.chat, `✧ Etiqueta o responde al mensaje del usuario que quieras Banear, Ejemplo:\n> */ban <@tag> <razón>*\n> */ban <razón>*`, m);
+        await conn.reply(m.chat, `✧ Etiqueta al usuario que quieras Banear.`, m);
         return;
     }
-    if (user === isOwner) return m.reply('✧ No puedes banear a mi Desarrollador.');
-
+    if (user === isROwner) return m.reply('✧ No puedes banear a mi Creador.');
     if (db[user]) {
         db[user].baneado = true;
         db[user].banRazon = `${reason}`;
-
         const nametag = await conn.getName(user);
         const nn = conn.getName(m.sender);
-
         await conn.reply(m.chat, `❀ El usuario *${nametag}* ha sido Baneado.\n> ✰ *Razón ⪼* ${reason}`, m, { mentionedJid: [user] });
-        conn.reply('120363284046748076@g.us', `✧ El usuario *${nametag}* ha sido Baneado por *${nn}*.\n> ✰ *Razón ⪼* ${reason}`, m, fake, );
+        await conn.reply('120363284046748076@g.us', `✧ El usuario *${nametag}* ha sido Baneado por *${nn}*.\n> ✰ *Razón ⪼* ${reason}`, m, fake, );
     } else {
-        await conn.reply(m.chat, `✧ El usuario no está registrado.`, m);
+        m.reply(m.chat, `✧ El usuario no está registrado.`);
     }
 };
 
