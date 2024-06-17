5c4ef5512563033ac2b7f10d4cf7b12229853edb
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 10:59:20 -0400

    Update MODS-unbanchat.js

--- plugins/MODS-unbanchat.js
+++ plugins/MODS-unbanchat.js
@@ -1,16 +1,11 @@
 var handler = async (m) => {
-if (m.sender === conn.user.jid) return;
-
 global.db.data.chats[m.chat].isBanned = false
 conn.reply(m.chat, `${global.botname} Ha sido activado de nuevo en el Chat.`, m, fake, )
-
 }
-
 handler.help = ['unbanchat']
 handler.registrado = true
 handler.tags = ['mods']
 handler.command = ['unbanchat']
 handler.mods = true 
 handler.group = true
-
 export default handler
