1409d51b61122013f2bb65d0c117fb10c2928c97
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 12:38:07 -0400

    Konichiwa

--- plugins/OWNER-newgc.js
+++ plugins/OWNER-newgc.js
@@ -1,6 +1,4 @@
 let handler = async (m, { conn, text }) => {
-if (m.sender === conn.user.jid) return
-
 if (!text) return m.reply('✧ Ingresa un nombre para el grupo.')
 try{
 m.reply('❀ Creando Grupo.')
@@ -11,12 +9,9 @@ m.reply('https://chat.whatsapp.com/' + url)
 m.reply(`✧ Ocurrió un error inesperado.`)
 }
 }
-
 handler.help = ['newgc <nombre>']
 handler.tags = ['owner']
 handler.command = ['newgc']
 handler.owner = true
 handler.registrado = true
-
-
 export default handler
