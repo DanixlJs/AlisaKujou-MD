d223a93189fe12217982eee51cea707e40a5d08d
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 09:57:27 -0400

    Update INTERNET-google.js

--- plugins/INTERNET-google.js
+++ plugins/INTERNET-google.js
@@ -2,15 +2,12 @@ import {googleIt} from '@bochilteam/scraper';
 import google from 'google-it';
 import axios from 'axios';
 let handler = async (m, { conn, command, args, usedPrefix }) => {
-
-if (m.sender === conn.user.jid) return;
-
   const fetch = (await import('node-fetch')).default;
   const text = args.join` `;
   if (!text) return conn.reply(m.chat, '✧ Ingrese un texto para realizar la búsqueda.', m);
 const url = 'https://google.com/search?q=' + encodeURIComponent(text);
 google({'query': text}).then(res => {
-let teks = `❀ *RESULTADOS DE* ${text}\n> → ${url}\n\n`
+let teks = `❀ *RESULTADOS DE* ${text}\n> ${url}\n\n`
 for (let g of res) {
 teks += `✰ *${g.title}*\n◈ ${g.link}\n◈ ${g.snippet}\n\n`
 } 
@@ -18,11 +15,9 @@ const ss = `https://image.thum.io/get/fullpage/${url}`
 conn.sendFile(m.chat, ss, 'error.png', teks, m)
 })
 } 
-
 handler.help = ['google <texto>'];
 handler.tags = ['internet'];
 handler.registrado = true;
 handler.command = ['google'];
 handler.diamantes = 1;
-
 export default handler;
