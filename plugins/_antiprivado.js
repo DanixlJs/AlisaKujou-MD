36cc4c3565ce9d2032b4781f2910e321d0764a1c
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 19:11:36 -0400

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -2,7 +2,6 @@ export async function before(m, {conn, isAdmin, isOwner}) {
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
-  if (m.text.includes('qr') || m.text.includes('get --code') || m.text.includes('qr --code') || m.text.includes('owner') || m.text.includes('grupos')) return !0;
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner) {
