60bbbf39c234f1193b3aa9ab1e5c171772123642
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 08:55:34 -0400

    Update DOWNLOADER-yt.js

--- plugins/DOWNLOADER-yt.js
+++ plugins/DOWNLOADER-yt.js
@@ -6,7 +6,7 @@ let user = global.db.data.users[m.sender]
 try {
 let { title, size, quality, thumbnail, dl_url } = await Scraper.ytmp4(args[0])
 if (size.includes('GB') || size.replace(' MB', '') > 500) { return await m.reply('✧ El archivo pesa mas de 500 MB, se canceló la Descarga.')}
-let txt = `❀ YOUTUBE DL\n ✰ *Titulo ⪼* ${title}\n◈ *Calidad ⪼* ${quality}\n◈ *Peso ⪼* ${size}\n\n> Descargando su archivo, espere un momento.`
+let txt = `❀ *YOUTUBE DL*\n ✰ *Titulo ⪼* ${title}\n◈ *Calidad ⪼* ${quality}\n◈ *Peso ⪼* ${size}\n> Descargando su archivo, espere un momento.`
 await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m)
 await conn.sendFile(m.chat, dl_url, title + '.mp4', `❀ Aqui tiene.`, m, false, { asDocument: user.useDocument })
 } catch (e){
