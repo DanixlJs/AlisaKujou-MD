fb339eec0cd9b14ab87b2f9e1527a9e6fd481356
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 12:20:42 -0400

    UwU

--- plugins/OWNER-delplugin.js
+++ plugins/OWNER-delplugin.js
@@ -1,16 +1,7 @@
 import { tmpdir } from 'os'
 import path, { join } from 'path'
-import {
-  readdirSync,
-  statSync,
-  unlinkSync,
-  existsSync,
-  readFileSync,
-  watch
-} from 'fs'
+import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch } from 'fs'
 let handler = async (m, { conn, usedPrefix: _p, command, __dirname, args, text }) => {
-if (m.sender === conn.user.jid) return
-
 let ar = Object.keys(plugins)
     let ar1 = ar.map(v => v.replace('.js', ''))
     if (!text) return conn.reply(m.chat, `✧ Ingrese el nombre del Plugin a eliminar.`, m)
@@ -18,11 +9,9 @@ let ar = Object.keys(plugins)
 const file = join(__dirname, '../plugins/' + args[0] + '.js')
 unlinkSync(file)
 conn.reply(m.chat, `❀ El Plugin *"${args[0]}.js"* ha sido eliminado correctamente.`)
-
 }
 handler.help = ['delplugin <archivo>']
 handler.tags = ['owner']
 handler.command = ['delplugin']
 handler.rowner = true
-
 export default handler
