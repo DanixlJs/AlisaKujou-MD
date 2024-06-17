541768a39b677eeaf24aeeee8bc147f18355a145
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 12:32:55 -0400

    UwU

--- plugins/OWNER-linksdb.js
+++ plugins/OWNER-linksdb.js
@@ -1,29 +1,23 @@
 import fs from 'fs';
 import path from 'path';
-
 const dbPath = './media/database/db.json';
 const loadDB = (path) => {
     return JSON.parse(fs.readFileSync(path, 'utf8'));
 };
-
 const handler = async (m, { conn, args }) => {
-if (m.sender === conn.user.jid) return;
     const category = args[0];
     if (!category) {
         await conn.reply(m.chat, '✧ Por favor, proporciona una categoría (video o imagen).', m);
         return;
     }
-
     const db = loadDB(dbPath);
     const links = db.links && db.links[category] ? db.links[category] : [];
     const count = links.length;
-
     await conn.reply(m.chat, `❀ Hay *${count}* links en la categoría *${category}*.`, m, fake, );
 };
-
 handler.help = ['linksdb <categoria>'];
 handler.tags = ['owner'];
+handler.owner = true;
 handler.command = ['linksdb'];
 handler.registrado = true;
-
 export default handler;
