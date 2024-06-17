df140100572db98984f39b9fd721af36b5eecb72
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 10:47:46 -0400

    Update MODS-get.js

--- plugins/MODS-get.js
+++ plugins/MODS-get.js
@@ -1,20 +1,14 @@
 import fetch from 'node-fetch';
-
 const handler = async (m, { conn, args, usedPrefix, command }) => {
-if (m.sender === conn.user.jid) return;
-
     if (!args[0]) {
         return conn.reply(m.chat, `✧ Ingresa un enlace para descargar la imagen o video.`, m);
     }
-
     const url = args[0];
     try {
         const response = await fetch(url);
         if (!response.ok) return m.reply('✧ Error al descargar el archivo.');
-
         const buffer = await response.buffer();
         const contentType = response.headers.get('content-type');
-
         if (contentType.startsWith('image/')) {
             await conn.sendMessage(m.chat, { image: buffer, caption: `❀ Imagen descargada desde:\n> *${url}*` }, { quoted: m });
         } else if (contentType.startsWith('video/')) {
@@ -27,11 +21,9 @@ if (m.sender === conn.user.jid) return;
         await conn.reply(m.chat, error.toString(), m);
     }
 };
-
 handler.help = ['g <link>'];
 handler.tags = ['mods'];
 handler.command = ['g'];
 handler.mods = true;
 handler.registrado = true;
-
 export default handler;
