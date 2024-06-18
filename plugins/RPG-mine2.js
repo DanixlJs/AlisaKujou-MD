48045897ceccf6c54ed98ee93056e0e14b82819e
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:48:18 -0400

    Update RPG-mine2.js

--- plugins/RPG-mine2.js
+++ plugins/RPG-mine2.js
@@ -1,25 +1,19 @@
 const handler = async (m, {conn}) => {
-if (m.sender === conn.user.jid) return;
     let user = global.db.data.users[m.sender];
     const d = Math.floor(Math.random() * 20);
     let msg = `❀ Realizaste tu turno en la mina y lograste minar la cantidad de *${d}* Diamante(s)`;
     const time = user.minetime2 + 600000;
     if (new Date - user.minetime2 < 600000) return m.reply(`✧ Espera *${msToTime(time - new Date())}* para volver a minar Diamantes.`)
-    
     conn.reply(m.chat, msg, m, fake,);
-    
     user.diamantes += d;
     user.minetime2 = new Date() * 1
 };
-
 handler.registrado = true;
 handler.help = ['minar2', 'mine2'];
 handler.command = ['minar2', 'mine2'];
 handler.group = true;
 handler.tags = ['rpg'];
-
 export default handler;
-
 function msToTime(duration) {
     const milliseconds = parseInt((duration % 1000) / 100);
     let seconds = Math.floor((duration / 1000) % 60);
@@ -29,5 +23,4 @@ function msToTime(duration) {
     minutes = (minutes < 10) ? '0' + minutes : minutes;
     seconds = (seconds < 10) ? '0' + seconds : seconds;
     return minutes + ' Minuto(s) ' + seconds + ' Segundo(s)';
-    
 }
