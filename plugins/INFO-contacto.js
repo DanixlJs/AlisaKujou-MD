c82e1a173f933677b3f87a98d4a26b421d19f700
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:14:38 -0400

    Update INFO-contacto.js

--- plugins/INFO-contacto.js
+++ plugins/INFO-contacto.js
@@ -1,7 +1,5 @@
 import PhoneNumber from 'awesome-phonenumber'
-
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
 let biografia = await conn.fetchStatus('595983799436' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
@@ -9,20 +7,16 @@ let biografiaBot = await conn.fetchStatus('595973755967' +'@s.whatsapp.net').cat
 let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 let name = await conn.getName(who)
-
   await sendContactArray(conn, m.chat, [
      [`595983799436`, `👑 Owner`, `🧑‍💻danixljs`, 'Solo Cosas referentes al Bot', 'josedanielcristaldovera023@gmail.com', `🇵🇾 Paraguay`, `https://chat.whatsapp.com/Kbj38zCqOvqH9KM5bRH1Hb`, bio],
 [`${conn.user.jid.split('@')[0]}`, `🤍 Bot OFC`, `${global.wm}`, `${global.dev}`, 'alisakujoumd@gmail.com', `🇵🇾 Paraguay`, `https://github.com/DanixlJs/AlisaKujou-MD`, biobot]
 ], m)
   } 
-
 handler.help = ["creador","owner"]
 handler.tags = ["info"]
 handler.command = ['creador', 'owner']
 handler.registrado = true
-
 export default handler
-
 async function sendContactArray(conn, jid, data, quoted, options) {
         if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
                 let contacts = []
