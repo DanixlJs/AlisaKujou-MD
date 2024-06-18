f06ce8b8f6f5864313cdb9b9c82b5aa81344c0d0
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:50:29 -0400

    Update RPG-myns.js

--- plugins/RPG-myns.js
+++ plugins/RPG-myns.js
@@ -1,16 +1,10 @@
 import { createHash } from 'crypto'
 let handler = async (m, { conn, text, usedPrefix }) => {
-if (m.sender === conn.user.jid) return
-
-if (!global.db.data.users[m.sender].registrado) return m.reply(`✧ Necesitas estar registrado para usar este comando.\n *${usedPrefix}reg <nombre.edad>*`)
-
 let sn = createHash('md5').update(m.sender).digest('hex')
-
 conn.reply(m.chat, sn, m, fake, )
 }
-
 handler.help = ['myns']
 handler.tags = ['rpg']
 handler.command = ['myns']
-
+handler.registrado = true
 export default handler
