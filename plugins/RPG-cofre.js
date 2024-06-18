722d8302050e467dbc91a0fcf1d007f289878266
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:42:34 -0400

    Update RPG-cofre.js

--- plugins/RPG-cofre.js
+++ plugins/RPG-cofre.js
@@ -1,47 +1,36 @@
 const handler = async (m, {isPrems, conn}) => {
-if (m.sender === conn.user.jid) return;
   const time = global.db.data.users[m.sender].cofretime + 86400000;
-  if (new Date - global.db.data.users[m.sender].cofretime < 86400000) throw `✧ Espera *${msToTime(time - new Date())}* para volver a reclamarlo`;
-
+  if (new Date - global.db.data.users[m.sender].cofretime < 86400000) return m.reply(`✧ Espera *${msToTime(time - new Date())}* para volver a reclamarlo`);
   const dia = Math.floor(Math.random() * 50);
   const alisacoins = Math.floor(Math.random() * 10000);
   const expp = Math.floor(Math.random() * 10000);
-
   global.db.data.users[m.sender].diamantes += dia;
   global.db.data.users[m.sender].alisacoins += alisacoins;
   global.db.data.users[m.sender].experiencia += expp;
-
   const texto = `❀ *COFRE DIARIO*
 ✰ Abriste tu cofre diario y obtuviste los siguientes recursos:
 > ◈ Diamantes ⪼ *${dia}* 
 > ◈ ${global.botcoins} ⪼ *${alisacoins}*
 > ◈ Experiencia ⪼ *${expp}*`;
-
   await conn.reply(m.chat, texto,m ,fake, );
   global.db.data.users[m.sender].cofretime = new Date * 1;
 };
-
 handler.help = ['cofre'];
 handler.tags = ['rpg'];
 handler.registrado = true;
 handler.command = ['cofre'];
 handler.level = 5;
-
 export default handler;
-
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)];
 }
-
 function msToTime(duration) {
   const milliseconds = parseInt((duration % 1000) / 100);
   let seconds = Math.floor((duration / 1000) % 60);
   let minutes = Math.floor((duration / (1000 * 60)) % 60);
   let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-
   hours = (hours < 10) ? '0' + hours : hours;
   minutes = (minutes < 10) ? '0' + minutes : minutes;
   seconds = (seconds < 10) ? '0' + seconds : seconds;
-
   return hours + ' Hora(s) ' + minutes + ' Minuto(s)';
 }
