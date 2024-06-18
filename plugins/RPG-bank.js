ec8dc9296c77805634a751587d985428af7059f1
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:38:42 -0400

    Update RPG-bank.js

--- plugins/RPG-bank.js
+++ plugins/RPG-bank.js
@@ -1,17 +1,13 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if ( m.sender === conn.user.jid) return;
   try {
     let user = global.db.data.users[m.sender];
     let [type, amount] = text.split(' ');
-
     if (command === 'dep' || command === 'd') {
       if (type === 'diamantes' || type === 'alisacoins') {
         if (!amount || isNaN(amount) || amount <= 0) {
           m.reply(`✧ Ingrese una cantidad válida de *${type}* para depositar.`);
         }
-
         amount = parseInt(amount);
-
         if (type === 'diamantes') {
           if (user.diamantes < amount) {
             m.reply(`✧ No tienes suficientes Diamantes.`);
@@ -25,17 +21,14 @@ if ( m.sender === conn.user.jid) return;
           user.alisacoins -= amount;
           user.bank2 = amount;
         }
-
         m.reply(`✧ Has depositado *${amount}* *${type}* en tu banco.`);
       } else if (type === 'all') {
         let totalDiamantes = user.diamantes;
         let totalAlisaCoins = user.alisacoins;
-
         user.bank = totalDiamantes;
         user.bank2 = totalAlisaCoins;
         user.diamantes = 0;
         user.alisacoins = 0;
-
         m.reply(`✧ Has depositado todos tus Diamantes y ${global.botcoins} en el banco.`);
       } else {
         m.reply(`✧ Asegurate de usarlo de la siguiente manera, Ejemplo:\n> *${usedPrefix + command} <tipo> <cantidad>*\n> *${usedPrefix + command} all*`);
@@ -47,10 +40,8 @@ if ( m.sender === conn.user.jid) return;
     console.log(error);
   }
 };
-
 handler.help = ['dep <tipo> <cantidad>', 'dep all'];
 handler.tags = ['rpg'];
 handler.command = ['dep'];
 handler.registrado = true;
-
 export default handler;
