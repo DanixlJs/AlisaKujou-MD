314c94bb2a9fe7dd329ce7d1a6e2b749b9444a1f
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 22:02:22 -0400

    Update RPG-tops.js

--- plugins/RPG-tops.js
+++ plugins/RPG-tops.js
@@ -1,28 +1,16 @@
 const handler = async (m, {conn, args, participants}) => {
-if ( m.sender === conn.user.jid) return;
   const users = Object.entries(global.db.data.users).map(([key, value]) => {
     return {...value, jid: key};
   });
-
   const sortedExp = users.map(toNumber('experiencia')).sort(sort('experiencia'));
-
   const sortedDiamond = users.map(toNumber('diamantes')).sort(sort('diamantes'));
-
   const sortedCoins = users.map(toNumber('alisacoins')).sort(sort('alisacoins'));
-
   const sortedLevel = users.map(toNumber('level')).sort(sort('level'));
-
-
   const usersExp = sortedExp.map(enumGetKey);
-
   const usersDiamond = sortedDiamond.map(enumGetKey);
-
   const usersCoins = sortedCoins.map(enumGetKey);
-
   const usersLevel = sortedLevel.map(enumGetKey);
-
   const len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 10)) : Math.min(10, sortedExp.length);
-
   const adventurePhrases = [
   "Lidera la aventura y forja tu camino hacia la cima.",
   "¡Desafía lo desconocido y alcanza nuevas alturas!",
@@ -33,9 +21,7 @@ if ( m.sender === conn.user.jid) return;
   "La emoción de la competencia te impulsa hacia adelante.",
   "Aventúrate y conquista los primeros lugares con determinación.",
 ];
-
   const randomAdventurePhrase = adventurePhrases[Math.floor(Math.random() * adventurePhrases.length)];
-
   const texto = `❀ *TABLA DE TOP AVENTUREROS*
 ✰ Top *${len}* Usuarios en Experiencia.
 ◈ *Tú posición ⪼* ${usersExp.indexOf(m.sender) + 1} de ${usersExp.length}
@@ -58,24 +44,19 @@ ${sortedCoins.slice(0, len).map(({jid, alisacoins}, i) => `${i + 1}. ${participa
 ${sortedLevel.slice(0, len).map(({jid, level}, i) => `${i + 1}. ${participants.some((p) => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]}\n→ Nivel *${level}*`).join`\n`}
 
 > ➤ *${randomAdventurePhrase}*`.trim();
-
   conn.sendMessage(m.chat, {text: texto, mentions: conn.parseMention(texto)}, {quoted: m})
 };
-
 handler.help = ['top'];
 handler.tags = ['rpg'];
 handler.command = ['leaderboard', 'lb', 'top'];
 handler.registrado = true
 handler.fail = null;
 handler.group = true;
-
 export default handler;
-
 function sort(property, ascending = true) {
   if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property];
   else return (...args) => args[ascending & 1] - args[!ascending & 1];
 }
-
 function toNumber(property, _default = 0) {
   if (property) {
     return (a, i, b) => {
@@ -83,7 +64,6 @@ function toNumber(property, _default = 0) {
     };
   } else return (a) => a === undefined ? _default : a;
 }
-
 function enumGetKey(a) {
   return a.jid;
 }
