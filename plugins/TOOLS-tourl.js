58c8abbebb7179009e8f7a98e81782fccbbc8d00
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 22:27:52 -0400

    Update TOOLS-tourl.js

--- plugins/TOOLS-tourl.js
+++ plugins/TOOLS-tourl.js
@@ -8,23 +8,17 @@ let name = await conn.getName(who)
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
 if (!mime) return m.reply('✧ Responde a un Video o Imagen.')
-
-m.react('✏️') 
 let media = await q.download()
 let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
 let link = await (isTele ? uploadImage : uploadFile)(media)
 let info = `❀ *RESULTADOS:*\n✰ *Enlace ⪼* ${link}\n◈ *Tamaño ⪼* ${media.length} Bytes\n◈ *Expira ⪼* ${isTele ? 'No Expira' : 'Desconocido'}\n◈ *Acortado ⪼* ${await shortUrl(link)}]`
-
 conn.reply(m.chat, info, m, fake, )
-
 }
 handler.help = ['tourl']
 handler.tags = ['tools']
 handler.registrado = true
 handler.command = ['tourl']
-
 export default handler
-
 async function shortUrl(url) {
 let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
 return await res.text()
