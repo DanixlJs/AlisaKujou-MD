1b76cc04d72f2b81abff8219554cffa0c631cb11
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:49:28 -0400

    Update RPG-mine3.js

--- plugins/RPG-mine3.js
+++ plugins/RPG-mine3.js
@@ -5,21 +5,16 @@ if (m.sender === conn.user.jid) return;
     let msg = `❀ En hora buena, minaste la cantidad de *${alisacoins}* AlisaCoins.`;
     const time = user.minetime3 + 600000;
     if (new Date - user.minetime3 < 600000) return m.reply(`✧ Espera *${msToTime(time - new Date())}* para volver a minar ${global.botcoins}`)
-    
     conn.reply(m.chat, msg, m, fake,);
-    
     user.alisacoins += alisacoins;
     user.minetime3 = new Date() * 1
 };
-
 handler.registrado = true;
 handler.help = ['minar3', 'mine3'];
 handler.command = ['minar3', 'mine3'];
 handler.group = true;
 handler.tags = ['rpg'];
-
 export default handler;
-
 function msToTime(duration) {
     const milliseconds = parseInt((duration % 1000) / 100);
     let seconds = Math.floor((duration / 1000) % 60);
