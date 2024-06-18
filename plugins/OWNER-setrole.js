a51d9a9b3f6c1d7d8452fb2991c93dfa16198eed
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:33:41 -0400

    Update OWNER-setrole.js

--- plugins/OWNER-setrole.js
+++ plugins/OWNER-setrole.js
@@ -1,10 +1,8 @@
 const handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if ( m.sender === conn.user.jid) return;
 if (command === 'setrole') {
     if (args.length >= 2) {
         const user = args[0].replace('@', '') + '@s.whatsapp.net';
         const role = args.slice(1).join(' ');
-        
         if (global.db.data.users[user]) {
             global.db.data.users[user].userol = role;
             await conn.reply(m.chat, `✧ El usuario @${user.replace('@s.whatsapp.net', '')} ha recibido el rol de ${role}.`, m, { mentions: [user]});
@@ -18,7 +16,6 @@ if (command === 'setrole') {
 if (command === 'delrole') {
     if (text) {
         const user = text.replace('@', '') + '@s.whatsapp.net';
-        
         if (global.db.data.users[user]) {
             global.db.data.users[user].userol = 'Usuario';
             await conn.reply(m.chat, `✧ El usuario @${user.replace('@s.whatsapp.net', '')} ha perdido su rol.`, m, { mentions: [user]});
@@ -26,13 +23,10 @@ if (command === 'delrole') {
         await conn.reply(m.chat, `✧ Etiqueta a la persona que quieras quitar su rol.`, m);
     }
 }
-
 };
-
 handler.help = ['setrole <@tag> <rol>', 'delrole'];
 handler.command = ['setrole', 'delrole'];
 handler.tags = ['owner'];
 handler.rowner = true;
 handler.group = true;
-
 export default handler;
