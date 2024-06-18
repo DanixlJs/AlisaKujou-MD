a54d5b4146d6e2f35619fcb84319b3a2aca5675c
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:30:35 -0400

    Update OWNER-setbotname.js

--- plugins/OWNER-setbotname.js
+++ plugins/OWNER-setbotname.js
@@ -1,5 +1,4 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if ( m.sender === conn.user.jid) return
   if (!text) return m.reply(`✧ Ingresa el nuevo nombre del Bot.`)
   try {
     await conn.updateProfileName(text)
@@ -9,11 +8,9 @@ if ( m.sender === conn.user.jid) return
     throw `✧ Ocurrió un error inesperado.`
   }
 }
-
 handler.help = ['setbotname']
 handler.tags = ['owner']
 handler.command = ['setbotname']
 handler.registrado = true
 handler.owner = true
-
 export default handler
