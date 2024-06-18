154e709c391dc5633035206cb6ebe790ad762bf9
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:31:19 -0400

    Update OWNER-setcmd.js

--- plugins/OWNER-setcmd.js
+++ plugins/OWNER-setcmd.js
@@ -1,8 +1,5 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if ( m.sender === conn.user.jid) return
-
   global.db.data.sticker = global.db.data.sticker || {};
-
   if (!m.quoted) return m.reply('✧ Responde a un Sticker para agregarle el Comando.');
   if (!m.quoted.fileSha256) return m.reply('✧ Responde a un Sticker o Imagen.');
   if (!text) return m.reply(`✧ Ingresa el comando que quieras poner.`);
@@ -12,10 +9,8 @@ if ( m.sender === conn.user.jid) return
   sticker[hash] = {text, mentionedJid: m.mentionedJid, creator: m.sender, at: + new Date, locked: false};
   m.reply(`❀ CMD agregado con éxito.`);
 };
-
 handler.command = ['setcmd', 'addcmd'];
 handler.tags = ['owner'];
 handler.help = ['setcmd <texto>'];
 handler.rowner = true;
-
 export default handler;
