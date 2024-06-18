0c9cc6073ad7ba58612cf45b3638bcab232639d8
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:02:17 -0400

    Update GRUPOS-tag.js

--- plugins/GRUPOS-tag.js
+++ plugins/GRUPOS-tag.js
@@ -1,13 +1,10 @@
 import MessageType from '@whiskeysockets/baileys'
 import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
-
 let handler = async (m, { conn, text, participants }) => {
 let users = participants.map(u => conn.decodeJid(u.id))
 let q = m.quoted ? m.quoted : m
 let c = m.quoted ? m.quoted : m.msg
-
  if (!text) return m.reply('✧ Ingresa un texto.')
- 
  const msg = conn.cMod(m.chat,
 generateWAMessageFromContent(m.chat, {
 [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
@@ -21,12 +18,10 @@ text || q.text, conn.user.jid, { mentions: users }
 )
 await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
-
 handler.help = ['hidetag <texto>', 'tag <texto>']
 handler.tags = ['grupo']
 handler.command = ['hidetag', 'tag'] 
 handler.group = true
 handler.admin = true
 handler.registrado = true
-
 export default handler 
