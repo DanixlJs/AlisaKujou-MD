def7d792bfbf7b0888bd947fe3e41c353eff54c2
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:45:56 -0400

    Update RPG-formarpareja.js

--- plugins/RPG-formarpareja.js
+++ plugins/RPG-formarpareja.js
@@ -1,25 +1,20 @@
 const toM = (a) => '@' + a.split('@')[0];
 function handler(m, {groupMetadata, conn}) {
-if (m.sender === conn.user.jid) return;
   const ps = groupMetadata.participants.map((v) => v.id);
   const a = ps.getRandom();
   let b;
   do b = ps.getRandom();
   while (b === a);
-
   let texto =`❀ *I LOVE YOU*
 ✰ ${toM(a)}
 ◈ Deberias Casarte con 
 ✰ ${toM(b)}
 > Hacen Una Bonita Pareja 💓`
-
    m.reply(texto, null, { mentions: [a, b], });
 }
-
 handler.help = ['formarpareja'];
 handler.tags = ['rpg'];
 handler.command = ['formarpareja', 'formarparejas'];
 handler.group = true;
 handler.registrado = true;
-
 export default handler;
