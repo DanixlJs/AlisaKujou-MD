0b6240cda58aa2c91ef3b3b45fa638578f6edbdf
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:39:52 -0400

    Update RPG-calculador.js

--- plugins/RPG-calculador.js
+++ plugins/RPG-calculador.js
@@ -1,11 +1,10 @@
 const handler = async (m, { conn, command, text, usedPrefix }) => {
-if ( m.sender === conn.user.jid) return;
   if (!text) m.reply(`✧ Mensiona a un Usuario.`);
   const percentages = (500).getRandom();
   let emoji = '';
   let description = '';
   switch (command) {
-    case 'gay2':
+    case 'gay':
       emoji = '🏳️‍🌈';
       if (percentages < 50) {
         description = `❀ Los calculos han arrojado que ${text.toUpperCase()} es *${percentages}%* Gay ${emoji}\n> ✰ Eso es bajo, ¡Tu eres Joto, no Gay!`;
@@ -110,11 +109,9 @@ var hawemod = [
  }
 loading()    
 };
-
-handler.help = ['gay2 <@tag> | <nombre>', 'lesbiana <@tag> | <nombre>', 'pajero <@tag> | <nombre>', 'pajera <@tag> | <nombre>', 'puto <@tag> | <nombre>', 'puta <@tag> | <nombre>', 'manco <@tag> | <nombre>', 'manca <@tag> | <nombre>', 'rata <@tag> | <nombre>', 'prostituta <@tag> | <nombre>', 'prostituto <@tag> | <nombre>'];
+handler.help = ['gay <@tag> | <nombre>', 'lesbiana <@tag> | <nombre>', 'pajero <@tag> | <nombre>', 'pajera <@tag> | <nombre>', 'puto <@tag> | <nombre>', 'puta <@tag> | <nombre>', 'manco <@tag> | <nombre>', 'manca <@tag> | <nombre>', 'rata <@tag> | <nombre>', 'prostituta <@tag> | <nombre>', 'prostituto <@tag> | <nombre>'];
 handler.tags = ['rpg'];
 handler.registrado = true;
 handler.group = true;
-handler.command = ['gay2', 'lesbiana', 'pajero', 'pajera', 'puto', 'puta', 'manco', 'manca', 'rata', 'prostituta', 'prostituto'];
-
+handler.command = ['gay', 'lesbiana', 'pajero', 'pajera', 'puto', 'puta', 'manco', 'manca', 'rata', 'prostituta', 'prostituto'];
 export default handler;
