7e4a2e5830f03e00e2a1301846ad36f60a3496c1
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:34:30 -0400

    Update OWNER-update.js

--- plugins/OWNER-update.js
+++ plugins/OWNER-update.js
@@ -1,24 +1,15 @@
 import { execSync } from 'child_process'
-
 var handler = async (m, { conn, text }) => {
-if ( m.sender === conn.user.jid) return
-
 m.react('🚀') 
 try {
-
 const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
 let messager = stdout.toString()
-
 if (messager.includes('Already up to date.')) messager = '❀ Ya estoy actualizada a la última versión.'
-
 if (messager.includes('Updating')) messager = '❀ *ACTUALIZACIÓN EXITOSA*\n\n' + stdout.toString()
 conn.reply(m.chat, messager, m, fake,)
-
 } catch { 
 try {
-
 const status = execSync('git status --porcelain')
-
 if (status.length > 0) {
 const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
 if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('Session/') || line.includes('npm-debug.log')) {
@@ -39,12 +30,9 @@ errorMessage2 += '\n✧ Mensaje de error: ' + error.message;
 await conn.reply(m.chat, errorMessage2, m, fake,)
 }
 }
-
 }
-
 handler.help = ['update', 'actualizar']
 handler.tags = ['owner']
 handler.command = ['update', 'actualizar']
 handler.rowner = true
-
 export default handler
