6232d43d4852b3841b92ee9d81cfda084293678f
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:40:41 -0400

    Update RPG-cf.js

--- plugins/RPG-cf.js
+++ plugins/RPG-cf.js
@@ -1,22 +1,16 @@
 const handler = async (m, { conn, args, usedPrefix }) => {
-if ( m.sender === conn.user.jid) return;
     const user = global.db.data.users[m.sender];
     const bet = parseInt(args[1]);
-
     if (user.alisacoins < bet) return m.reply(`✧ No tienes esa cantidad de *${bet}* para apostar, solo tienes *${user.alisacoins}* ${global.botcoins}`);
-
     if (user.alisacoins < 200) {
         await conn.reply(m.chat, `✧ Te quedaste sin ${global.botcoins} y el mínimo para apostar es *200*.`, m);
         return;
     }
-
     if (!args[0] || !['cara', 'cruz'].includes(args[0]) || isNaN(bet) || bet < 200) {
         await conn.reply(m.chat, `✧ Elige una opción para empezar a apostar, Ejemplo:\n> *${usedPrefix}cf cruz 200*\n◈ *Nota ⪼* La apuesta mínima es de 200 ${global.botcoins}`, m);
         return;
     }
-
     const result = Math.random() < 0.5 ? 'cara' : 'cruz';
-
     if (args[0] === result) {
         const payout = bet * 2;
         user.alisacoins += payout;
@@ -25,13 +19,10 @@ if ( m.sender === conn.user.jid) return;
         user.alisacoins -= bet;
         await conn.reply(m.chat, `✧ La moneda cayó en *${result}* y perdiste *-${bet}* ${global.botcoins}`, m);
     }
-
     global.db.data.users[m.sender] = user;
 };
-
 handler.help = ['cf <cara|cruz> <apuesta>'];
 handler.command = ['cf', 'coinflip'];
 handler.tags = ['rpg'];
 handler.registrado = true;
-
 export default handler;
