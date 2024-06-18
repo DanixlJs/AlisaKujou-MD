5d54cfb944be730ea08fa640b2f5aa4b65d45db8
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:59:57 -0400

    Update RPG-retirar.js

--- plugins/RPG-retirar.js
+++ plugins/RPG-retirar.js
@@ -1,17 +1,13 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if ( m.sender === conn.user.jid) return;
   try {
     let user = global.db.data.users[m.sender];
     let [type, amount] = text.split(' ');
-
     if (command === 'retirar' || command === 'with') {
       if (type === 'diamantes' || type === 'alisacoins') {
         if (!amount || isNaN(amount) || amount <= 0) {
           m.reply(`✧ Ingrese una cantidad válida de *${type}* para retirar.`);
         }
-
         amount = parseInt(amount);
-
         if (type === 'diamantes') {
           if (user.diamantes < amount) {
             m.reply(`✧ No tienes esa cantidad en el Banco.`);
@@ -25,17 +21,14 @@ if ( m.sender === conn.user.jid) return;
           user.alisacoins += amount;
           user.bank2 -= amount;
         }
-
         m.reply(`✧ Has retirado *${amount}* *${type}* del banco.`);
       } else if (type === 'all') {
         let totaldiamantes = user.bank;
         let totalcoins = user.bank2;
-
         user.diamantes = totaldiamantes;
         user.alisacoins = totalcoins;
         user.bank = 0;
         user.bank2 = 0;
-
         m.reply(`✧ Has retirado todos tus Diamantes y ${global.botcoins} del banco.`);
       } else {
         m.reply(`✧ Asegurate de usarlo de la siguiente manera, Ejemplo:\n> *${usedPrefix + command} <tipo> <cantidad>*\n> *${usedPrefix + command} all*`);
@@ -47,10 +40,8 @@ if ( m.sender === conn.user.jid) return;
     console.log(error);
   }
 };
-
 handler.help = ['retirar <tipo> <cantidad>', 'retirar all'];
 handler.tags = ['rpg'];
 handler.command = ['retirar', 'with'];
 handler.registrado = true;
-
 export default handler;
