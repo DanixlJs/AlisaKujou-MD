680c068d4b1971db24fdf20f7bf8bbb8dc77170f
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-17 21:02:50 -0400

    Update GRUPOS-tagall.js

--- plugins/GRUPOS-tagall.js
+++ plugins/GRUPOS-tagall.js
@@ -8,16 +8,13 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   const oi = `✰ *Mensaje ⪼* ${pesan}`;
   let teks = `❀ *INVOCANDO GRUPO*\n${oi}\n\n◈ *Tag:*\n`;
   for (const mem of participants) {
-    teks += `> → @${mem.id.split('@')[0]}\n`;
+    teks += `> @${mem.id.split('@')[0]}\n`;
   }
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
-
-
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
 handler.tags = ['grupo'];
 handler.command = ['tagall', 'invocar'];
 handler.admin = true;
 handler.group = true;
-
 export default handler;
