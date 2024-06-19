1f79b5cfd4c276c3e8632119af11d6815181b62d
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-18 22:39:07 -0400

    test
    awita de UwU :3

---
+++ plugins/OWNER-delprem.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  if (!text) return m.reply(`✧ Etiqueta a un usuario.`);
+  
+  let [mentionedJid] = m.mentionedJid || [];
+  if (!mentionedJid) return m.reply('✧ Por favor, menciona a un usuario.');
+
+  if (!global.db.data.users[mentionedJid]) global.db.data.users[mentionedJid] = {};
+  
+  global.db.data.users[mentionedJid].premium = false;
+  global.db.data.users[mentionedJid].premiumTime = 0;
+
+  conn.reply(m.chat, `✧ @${mentionedJid.split('@')[0]} ya no es un usuario premium.`, m, fake, { mentions: [mentionedJid]} );
+};
+
+handler.help = ['delprem <@tag>'];
+handler.tags = ['owner'];
+handler.command = ['delprem'];
+handler.rowner = true;
+handler.registrado = true;
+
+export default handler;
