6058e4e979daef4794b97fbe27cfd38658717fb9
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-19 09:20:47 -0400

    Actualizar OWNER-prefix.js

--- plugins/OWNER-prefix.js
+++ plugins/OWNER-prefix.js
@@ -1,16 +1,16 @@
 const handler = async (m, {conn, command, text, isOwner}) => {
-  if (isOwner) {
-					return
+if (isOwner) {
+return
 }
-		if (command === 'prefix') {
-		if (!text) return m.reply('✧ Ingresa un Prefijo para Alisa')
-		global.prefix = [`${text}`]
-		if (text.length > 1) return m.reply('✧ El prefijo no puede tener más de 1 carácteres.')
-		await conn.reply(m.chat, `❀ Se cambió el Prefijo a *[ ${text} ]*, m, fake, )`
+if (command === 'prefix') {
+if (!text) return m.reply('✧ Ingresa un Prefijo para Alisa')
+if (text.length > 1) return m.reply('✧ El prefijo no puede tener más de 1 carácteres.')
+global.prefix = [`${text}`]
+await conn.reply(m.chat, `❀ Se cambió el Prefijo a *[ ${text} ]*`, m, fake, )
 }
-		if (command === 'resetprefix') {
-		global.prefix = new RegExp('^[/.#!]')
-		await conn.reply(m.chat, '❀ Se ha reseteado el prefijo de Alisa', m, fake, )
+if (command === 'resetprefix') {
+global.prefix = new RegExp('^[/.#!]')
+await conn.reply(m.chat, '❀ Se ha reseteado el prefijo de Alisa', m, fake, )
 }
 
 handler.tags = ['owner']
