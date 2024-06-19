e7157598de7b9eff320782c65d524248f5b44848
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-19 11:23:29 -0400

    Update OWNER-tag.js

--- plugins/OWNER-tag.js
+++ plugins/OWNER-tag.js
@@ -1,11 +1,10 @@
-var handler = async (m, { conn }) => {
-  const sender = m.sender;
-  const tagMessage = `@${sender.split('@')[0]}`;
-  await conn.reply(m.chat, tagMessage, m, {
+var handler = async (m, { conn, text, isOwner }) => {
+  if (!isOwner) return;
+  const sender = text.replace('@', '') + '@s.whatsapp.net'.trim();
+  const tag = `@${sender.split('@')[0]}`;
+  await conn.reply(m.chat, tag, m, {
     mentions: [sender]
   });
 }
 handler.command = ['taguser'];
-handler.owner= true;
-handler.registrado = true;
 export default handler;
