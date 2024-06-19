5bca15e587bb36210f83170ade66db724225794f
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-19 11:16:29 -0400

    Delete plugins/_muto.js

--- plugins/_muto.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-import path from 'path'
-let handler = async (m, { conn }) => {
-  if (!global.db.data.users[m.sender]) global.db.data.users[m.sender] = {}
-  if (global.db.data.users[m.sender].muto) {
-    await conn.sendMessage(m.chat, { delete: { remoteJid: m.key.remoteJid, fromMe: false, id: m.key.id, participant: m.sender } })
-  }
-}
-export default handler
