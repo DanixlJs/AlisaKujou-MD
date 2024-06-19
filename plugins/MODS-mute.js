42a0e8aebed95d01b06cab9c13603b5e58686900
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-19 10:38:19 -0400

    Update MODS-mute.js

--- plugins/MODS-mute.js
+++ plugins/MODS-mute.js
@@ -1,22 +1,24 @@
 let handler = async (m, { conn, text, args, usedPrefix, command, isOwner }) => {
   if (command === 'mute') {
-  if (!text) return m.reply(`Etiqueta a la persona que quieras mutear.`)
-  let [mentionedJid] = m.mentionedJid || []
-  if (!mentionedJid) return m.reply('Por favor, etiqueta a un usuario.')
-  if (mentionedJid === isOwner) return m.reply('No puedes mutear a mi Desarrollador.')
+  if (!text) return m.reply(`✧ Etiqueta a la persona que quieras mutear.`)
+  let user = text.replace('@', '').trim()
+  let mentionedJid = `${user}@s.whatsapp.net`
+  if (!mentionedJid) return m.reply('✧ Por favor, etiqueta a un usuario.')
+  if (mentionedJid === isOwner) return m.reply('✧ No puedes mutear a mi Desarrollador.')
   if (!global.db.data.users[mentionedJid]) global.db.data.users[mentionedJid] = {}
   global.db.data.users[mentionedJid].muto = true
   let taguser = conn.getName(mentionedJid)
-  conn.reply(`*${taguser}* ha sido muteado.`)
+  conn.reply(`❀ *${taguser}* ha sido muteado.`)
 }
   if (command === 'unmute') {
-  if (!text) return m.reply(`Etiqueta a la persona que quieras desmutear.`)
-  let [mentionedJid] = m.mentionedJid || []
-  if (!mentionedJid) return m.reply('Por favor, etiqueta a un usuario.')
+  if (!text) return m.reply(`✧ Etiqueta a la persona que quieras desmutear.`)
+  let user = text.replace('@', '').trim()
+  let mentionedJid = `${user}@s.whatsapp.net`
+  if (!mentionedJid) return m.reply('✧ Por favor, etiqueta a un usuario.')
   if (!global.db.data.users[mentionedJid]) global.db.data.users[mentionedJid] = {}
   global.db.data.users[mentionedJid].muto = false
   let taguser = conn.getName(mentionedJid)
-  conn.reply(`*${taguser}* ha sido desmuteado.`)
+  conn.reply(`❀ *${taguser}* ha sido desmuteado.`)
 }
 }
 
