2bc93f17166c6933e5b68fad6622be87ac707ff6
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-18 23:53:23 -0400

    test

---
+++ plugins/RPG-pfp.js
@@ -0,0 +1,22 @@
+const handler = async (m, { conn, command }) => {
+  let mentionedJid = [];
+  if (m.message.extendedTextMessage.contextInfo.mentionedJid) {
+    mentionedJid = m.message.extendedTextMessage.contextInfo.mentionedJid;
+  }
+  let target = mentionedJid[0] || m.quoted.sender;
+  if (!target) return m.reply('✧ Por favor, etiqueta a alguien o responde a su mensaje.');
+
+  try {
+    const profilePicUrl = await conn.getProfilePicture(target);
+    await conn.sendMessage(m.chat, { image: { url: profilePicUrl }, caption: '' });
+  } catch (e) {
+    m.reply('✧ Lo siento, no pude obtener la foto de perfil.');
+  }
+};
+
+handler.command = ['pfp'];
+handler.help = ['pfp <@tag>];
+handler.registrado = true;
+handler.group = true;
+
+export default pfpHandler;
