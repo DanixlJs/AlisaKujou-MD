c18d58ba4ef47720b427b6af544490ba23bc652b
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-20 07:02:16 +0200

    Update RPG-say.js

--- plugins/RPG-say.js
+++ plugins/RPG-say.js
@@ -1,4 +1,5 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
+  if (!global.db.data.chats[m.chat].game) return m.reply('✧ Los comandos de RPG han sido desactivados en este grupo.');
 if (!text) return m.reply(`✧ Ingresa un texto.`);
 let msg = text
 conn.reply(m.chat, msg, null, { mentions: conn.parseMention(msg) });
