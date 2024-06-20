9bd66247d359809947d759e3db125f292f493b1e
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-20 06:57:11 +0200

    Update RPG-delttt.js

--- plugins/RPG-delttt.js
+++ plugins/RPG-delttt.js
@@ -1,5 +1,6 @@
 import MessageType from '@whiskeysockets/baileys';
 const handler = async (m, {conn, usedPrefix, command}) => {
+  if (!global.db.data.chats[m.chat].game) return m.reply('✧ Los comandos de RPG han sido desactivados en este grupo.');
   const room = Object.values(conn.game).find((room) => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender));
   if (room == undefined) return conn.reply(m.chat, `✧ No estás en ninguna sala, usa *${usedPrefix}ttt <texto>* para crear una nueva.`, m, fake, );
   delete conn.game[room.id];
