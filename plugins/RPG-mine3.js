0ecb7b71c71f3ce4d0ff8c487ae163c1c5192fd8
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-20 06:59:15 +0200

    Update RPG-mine3.js

--- plugins/RPG-mine3.js
+++ plugins/RPG-mine3.js
@@ -1,4 +1,5 @@
 const handler = async (m, {conn}) => {
+  if (!global.db.data.chats[m.chat].game) return m.reply('✧ Los comandos de RPG han sido desactivados en este grupo.');
 if (m.sender === conn.user.jid) return;
     let user = global.db.data.users[m.sender];
     const alisacoins = Math.floor(Math.random() * 10000);
