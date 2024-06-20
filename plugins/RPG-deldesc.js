04edb2ec2d15f9169d3f49d410b081ebe470629e
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-20 06:56:59 +0200

    Update RPG-deldesc.js

--- plugins/RPG-deldesc.js
+++ plugins/RPG-deldesc.js
@@ -1,4 +1,5 @@
 const handler = async (m, {conn, text}) => {
+  if (!global.db.data.chats[m.chat].game) return m.reply('✧ Los comandos de RPG han sido desactivados en este grupo.')
 let user = global.db.data.users[m.sender]
 user.desc = "";
 conn.reply(m.chat, '❀ Se eliminó tu descripción', m, )
