f907a921b4f4a7f82bb178d5449102b25102ee3b
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-20 09:00:31 +0200

    Actualizar testgacha.js

--- plugins/testgacha.js
+++ plugins/testgacha.js
@@ -1,65 +1,72 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'fs';
+import path from 'path';
 
-const db = './src/gacha/database.json'
+const dbPath = './src/gacha/database.json';
+
 const db_load = (path) => {
-    return JSON.parse(fs.readFileSync(path, 'utf8'))
+    return JSON.parse(fs.readFileSync(path, 'utf8'));
 }
-const db_save = (path, data) => {
-    fs.writeFileSync(path, JSON.stringify(data, null, 2))
-}
-let series = db_load(db)
 
-let characteres = db_load(db)
+const db_save = (path, data) => {
+    fs.writeFileSync(path, JSON.stringify(data, null, 2));
+}
+
+let series = db_load(dbPath);
 
 const handler = async (m, {conn, args, command, usedPrefix}) => {
     if (command === 'adds') {
-    if (args.length < 2) {
-        await m.reply(`Asegurate de usarlo de la siguiente manera, Ejmplo:\n> *${usedPrefix + command} <nombre> = <tag>*`)
-        return
-    }
-
-    const [serie, tag] = args.join(' ').split('=').map((str) => str.trim())
-    const id = Math.floor(100000 + Math.random() * 900000)
-    const anime = {
-        name: serie,
-        tag: tag,
-        id: id,
-        characteres: [],
-    }
-
-    series.push(anime)
-    db_save(db, series)
-    await conn.reply(m.chat, `${serie} Se agregó correctamente a la Base de Datos.`)
-}
-    if (command === 'addc') {
         if (args.length < 2) {
-            await m.reply(`Asegurate de usarlo de la siguiente manera, Ejemplo:\n> *${usedPrefix + command} <id> <character> = <tag>*`)
-            return
+            await m.reply(`✧ Asegúrate de usarlo de la siguiente manera, Ejemplo:\n> *${usedPrefix + command} <nombre> = <tag>*`);
+            return;
         }
 
-    const aid = args[0]
-    const ct = args.slice(1).join(' ').split('\n').map(line => line.split('=').map(str => str.trim()))
+        const [serie, tag] = args.join(' ').split('=').map((str) => str.trim());
+        const id = Math.floor(100000 + Math.random() * 900000);
+        const anime = {
+            name: serie,
+            tag: tag,
+            id: id,
+            characters: [],
+        }
 
-    for (const [cname, ctag] of ct) {
+        series.push(anime);
+        db_save(dbPath, series);
+        await conn.reply(m.chat, `❀ *${serie}* se agregó correctamente a la Base de Datos.`);
+    }
+
+    if (command === 'addc') {
+        if (args.length < 3) {
+            await m.reply(`✧ Asegúrate de usarlo de la siguiente manera, Ejemplo:\n> *${usedPrefix + command} <id> <nombre> = <tag>*`);
+            return;
+        }
+
+        const aid = args[0];
+        const [name, tag] = args.slice(1).join(' ').split('=').map(str => str.trim());
+        const anime = series.find(anime => anime.id === aid);
+        if (!anime) {
+            await m.reply(`✧ No se encontró una serie con el ID de *${aid}*.`);
+            return;
+        }
+
         const character = {
-            cname: cname,
-            ctag: ctag,
+            name: name,
+            tag: tag,
             aid: aid,
             value: 50,
             claimed: false,
             voteTime: 0,
         }
-    characteres.push(character)
-    }
-    db_save(db, characteres)
-    await m.reply(`Personajes agregados al Anime con ID *${aid}*`)
+
+        anime.characters.push(character);
+
+        db_save(dbPath, series);
+        await m.reply(`❀ Personaje agregado al anime *${anime.name}* con ID *${aid}*`);
     }
 }
 
-handler.command = ['adds', 'addc']
-handler.help = ['adds <name> = <tag>', 'addc <id> <name> = <tag>']
-handler.registrado = true
-handler.rowner = true
+handler.command = ['adds', 'addc'];
+handler.help = ['adds <nombre> = <tag>', 'addc <id> <nombre> = <tag>'];
+handler.registrado = true;
+handler.rowner = true;
 
-export default handler
+export default handler;
