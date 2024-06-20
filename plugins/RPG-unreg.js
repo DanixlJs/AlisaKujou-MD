5c28af93281a46480745807b1d65e823d75238ac
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-20 07:03:26 +0200

    Update RPG-unreg.js

--- plugins/RPG-unreg.js
+++ plugins/RPG-unreg.js
@@ -1,5 +1,6 @@
 import {createHash} from 'crypto';
 const handler = async function(m, {args, conn, usedPrefix}) {
+  if (!global.db.data.chats[m.chat].game) return m.reply('✧ Los comandos de RPG han sido desactivados en este grupo.');
   if (!args[0]) return m.reply(`✧ Ingrese su número de serie.`);
   const user = global.db.data.users[m.sender];
   const sn = createHash('md5').update(m.sender).digest('hex');
