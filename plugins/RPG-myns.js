888eb21ef504970dca87f915faea4fc6ca2eb03c
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-20 06:59:29 +0200

    Update RPG-myns.js

--- plugins/RPG-myns.js
+++ plugins/RPG-myns.js
@@ -1,5 +1,6 @@
 import { createHash } from 'crypto'
 let handler = async (m, { conn, text, usedPrefix }) => {
+  if (!global.db.data.chats[m.chat].game) return m.reply('✧ Los comandos de RPG han sido desactivados en este grupo.')
 let sn = createHash('md5').update(m.sender).digest('hex')
 conn.reply(m.chat, sn, m, fake, )
 }
