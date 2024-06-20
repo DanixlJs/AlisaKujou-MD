fc4cd4b51013a8c740d082dc44797e0fc051542e
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-19 23:10:31 -0400

    Actualizar test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,25 +1,21 @@
 import axios from 'axios';
-
 let handler = async (m, { conn, text }) => {
 if (!text) return conn.reply(m.chat, '✧ Ingresa un texto para realizar la búsqueda.', m);
-
 const getRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
-
 try {
     const { data } = await axios.get(`https://apis-starlights-team-cbb6f3a3.koyeb.app/starlight/tiktoksearch?text=${text}`);
     const results = data.data;
-
-    if (!results.length) return conn.reply(m.chat, 'No se encontraron resultados.', m);
-
+    if (!results.length) return conn.reply(m.chat, '✧ No se encontraron resultados.', m);
     const randomResult = getRandomElement(results);
     const videoUrl = randomResult.nowm;
-
-    await conn.sendFile(m.chat, videoUrl, 'tts.mp4', null, m);
-
+    await conn.sendFile(m.chat, videoUrl, 'tts.mp4', `❀ *TIKTOK SEARCH*\n✰ *Resultados para:*\n ${text}`, m);
 } catch (error) {
-    conn.reply(m.chat, `Error: ${error.message}`, m);
+    console.log(error);
 }
 };
 
-handler.command = ['test'];
+handler.command = ['tts', 'tiktokvid', 'tiktoksearch'];
+handler.help = ['tiktoksearch <texto>'];
+handler.registrado = true;
+handler.diamantes = 1;
 export default handler;
