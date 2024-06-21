5501a57873113450aa658fc9d061ccbc3a5b3718
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-21 04:59:08 +0200

    Update INFO-reporte.js

--- plugins/INFO-reporte.js
+++ plugins/INFO-reporte.js
@@ -5,21 +5,18 @@ const handler = async (m, {conn, text, usedPrefix, command, groupMetadata}) => {
   if (!text) return m.reply(`✧ Por favor escribe el reporte que quieras enviar a los Moderadores.`);
   if (text.length < 10) return m.reply(`✧ El reporte es demasiado corto, mínimo 10 caracteres.`);
   if (text.length > 1000) return m.reply(`✧ El límite del reporte es de 1000 caracteres.`);
-  const teks = `❀ 𝗥𝗘𝗣𝗢𝗥𝗧𝗘 𝗥𝗘𝗖𝗜𝗕𝗜𝗗𝗢\n\n✰ *Usuario ⪼* wa.me/${m.sender.split`@`[0]}\n◈ *Mensaje ⪼* ${text}\n◈ *Grupo ⪼* ${groupMetadata.subject}\n◈ *ID ⪼* ${groupMetadata.id}\n\n➤ 𝗜𝗡𝗙𝗢 𝗕𝗢𝗧\n✰ *Nombre ⪼* ${global.botname}\n◈ *Versión ⪼* ${global.vs}`;
-conn.reply('595983799436@s.whatsapp.net', teks, m, fake, );
-conn.reply('120363284046748076@g.us', teks, m, fake, );
+  const teks = `❀ *REPORTE RECIBIDO*\n\n✰ *Usuario ⪼* @${m.sender.replace('@s.whatsapp.net', '').trim()}\n◈ *Mensaje ⪼* ${text}\n◈ *Grupo ⪼* ${groupMetadata.subject}\n◈ *ID ⪼* ${groupMetadata.id}\n\n➤ *INFO BOT*\n✰ *Nombre ⪼* ${global.botname}\n◈ *Versión ⪼* ${global.vs}`;
+conn.reply('595983799436@s.whatsapp.net', teks, m, { mentions: [m.sender]} );
+conn.reply('120363284046748076@g.us', teks, m, { mentions: [m.sender]} );
   conn.reply(m.chat, '❀ El reporte se envió correctamente a los Moderadores, de ser necesario recibirá una respuesta.', m, fake, );
   user.reportime = new Date * 1;
 };
-
 handler.help = ['report <texto>', 'reportar <texto>'];
 handler.tags = ['info'];
 handler.command = ['report', 'reportar'];
 handler.group = true,
 handler.registrado = true;
-
 export default handler;
-
 function msToTime(duration) {
   const milliseconds = parseInt((duration % 1000) / 100);
   let seconds = Math.floor((duration / 1000) % 60);
