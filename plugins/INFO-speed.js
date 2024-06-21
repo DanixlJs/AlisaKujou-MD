560112a0a7e92cca70d3890fc470a0ace334abd7
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-21 05:01:14 +0200

    Update INFO-speed.js

--- plugins/INFO-speed.js
+++ plugins/INFO-speed.js
@@ -1,24 +1,12 @@
 import speed from 'performance-now'
-
 var handler = async (m, { conn }) => {
 let timestamp = speed()
 let latensi = speed() - timestamp
-let texto = `❀ *${global.botname}*
-✰ *Velocidad:*
-→ ${latensi.toFixed(4)}`.trim()
-m.react('✈️')
+let texto = `❀ *Speed ⪼* ${latensi.toFixed(4)}ms`
 conn.reply(m.chat, texto, m, fake, )
-
 }
 handler.help = ['speed']
 handler.tags = ['info']
 handler.command = ['speed']
 handler.registrado = true
-
 export default handler
-
-function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
