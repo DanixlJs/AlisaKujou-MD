0dae238324c44f72134092e91d8ff63e078ac096
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-21 04:54:52 +0200

    Update INFO-premium.js

--- plugins/INFO-premium.js
+++ plugins/INFO-premium.js
@@ -3,10 +3,9 @@ let handler = async (m, { conn, text, usedPrefix, command, isPrems}) => {
      let noPrem = `❀ Puedes adquirir tu Pase Premium en nuestro Patreon.\n> ${global.patreon}\n\n✰ Con tu Compra recibiras un Pase Premium y tambien los siguientes Articulos.\n◈ Diamantes\n◈ AlisaCoins\n◈ Experiencia\n◈ Y mas.`;
      if (!isPrems) conn.reply(m.chat, `${noPrem}`, m, fake, );
 }
-
 handler.registrado = true;
 handler.help = ['premium'];
 handler.command = ['premium'];
 handler.tags = ['info'];
-
+handler.disabled = true;
 export default handler;
