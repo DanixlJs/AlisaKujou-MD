9280c3c534c119298710a08d30c35fa48c9d22d0
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-21 03:51:11 +0200

    Update

--- plugins/GRUPOS-demote.js
+++ plugins/GRUPOS-demote.js
@@ -1,25 +1,13 @@
-var handler = async (m, { conn,usedPrefix, command, text }) => {
-if (isNaN(text) && !text.match(/@/g)){
-} else if (isNaN(text)) {
-var number = text.split`@`[1]
-} else if (!isNaN(text)) {
-var number = text
+let handler = async (m, { conn,usedPrefix, command, text, isOwner }) => {
+if (!m.mentionedJid[0] && !m.quoted) {
+return m.reply('✧ Etiqueta al usuario que quieras degradar.')
 }
-if (!text && !m.quoted) return m.reply(`✧ Etiqueta al usuario que quieras degradar.`)
-if (number.length > 13 || (number.length < 11 && number.length > 0)) return m.reply(`✧ El número no es válido.`)
-try {
-if (text) {
-var user = number + '@s.whatsapp.net'
-} else if (m.quoted.sender) {
-var user = m.quoted.sender
-} else if (m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-} 
-} catch (e) {
-} finally {
+let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+if (user === isOwner) {
+return m.reply('✧ No puedes degradar a mi Desarrollador'))
+}
 conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-conn.reply(m.chat, `❀ Hecho.`, m, fake, )
-}
+m.react('✅')
 }
 handler.help = ['demote <@tag>']
 handler.tags = ['grupo']
