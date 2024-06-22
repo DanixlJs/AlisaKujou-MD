2f56fae5e0c29b1154ebb5772aa4e3b93f894704
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-22 20:04:01 +0200

    Update MAIN-allfake.js

--- plugins/MAIN-allfake.js
+++ plugins/MAIN-allfake.js
@@ -17,11 +17,11 @@ headers: {
 'Upgrade-Insecure-Request': 1
 },
 ...options,
-responseType: 'arraybuffer'
+responseType: 'arrayBuffer'
 })
 return res.data
 } catch (e) {
-console.log(`✧ Error: ${e}`)
+console.log(`[ ✧ ] ERROR:\n`, e)
 }}
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
 global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
@@ -32,7 +32,6 @@ var l4 = 'https://instagram.com/';
 var l5 = 'https://facebook.com/';
 var l6 = 'https://github.com/';
 global.fakeLink = [l1, l2, l3, l4, l5, l6].getRandom()
-global.canales = [channel, channel2].getRandom()
 let bot = global.db.data.settings[this.user.jid];
 global.botname = bot.botname;
 global.wm = bot.botwm;
@@ -43,35 +42,16 @@ global.packname = 'ᰔᩚ 𝐒𝐭𝐢𝐜𝐤𝐞𝐫 𝐛𝐲';
 global.author = `${global.botname} ᰔᩚ`;
 global.botcommandcount = bot.botcommandCount;
 let category = "imagen"
- const dbPath = './media/database/db.json'
-  const dbData = JSON.parse(fs.readFileSync(dbPath))
-  const randomIndex = Math.floor(Math.random() * dbData.links[category].length)
-  const randomImagen = dbData.links[category][randomIndex]
-  const response = await fetch(randomImagen)
-  const rimg = await response.buffer()
+ const db = './media/database/db.json'
+  const db_ = JSON.parse(fs.readFileSync(db))
+  const random = Math.floor(Math.random() * db_.links[category].length)
+  const randomlink = db_.links[category][random]
+  const response = await fetch(randomlink)
+  const rimg = await response.arrayBuffer()
   global.icons = rimg
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "120363220559890200@g.us" } : {}) }, message: { orderMessage: { itemCount : 2024, status: 1, surface : 1, message: global.wm, orderTitle: 'Bang', thumbnail: global.icons, sellerJid: '0@s.whatsapp.net' }}}
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "595983799436@s.whatsapp.net" } : {}) }, message: { orderMessage: { itemCount : 2024, status: 1, surface : 1, message: `${global.wm}\n${global.dev}`, orderTitle: 'Bang', thumbnail: global.icons, sellerJid: '0@s.whatsapp.net' }}}
 var credit = 'X8KpIFJlcXVlc3RlZCBieSBDdXJpb3NpdHlCb3Rf'
 global.cred = Buffer.from(credit, 'base64')
-global.rcanal = {
-contextInfo: {
-isForwarded: true,
-forwardedNewsletterMessageInfo: {
-newsletterJid: randomChannel.id,
-serverMessageId: 100,
-newsletterName: randomChannel.nombre,
-},
-},
-}
-let canalId = ["120363302120826314@newsletter", "120363314021119064@newsletter"]
-let canalNombre = [`${global.wm}`, "♋︎ 𝐃𝙴𝚅 𝐖𝙾𝚁𝙻𝙳 - 𝐓𝙴𝙰𝙼 ♋︎"]
-async function getRandomChannel() {
-let randomIndex = Math.floor(Math.random() * canalId.length)
-let id = canalId[randomIndex]
-let nombre = canalNombre[randomIndex]
-return { id, nombre }
-} 
-let randomChannel = await getRandomChannel()
-global.fake = { contextInfo: { mentionedJid: conn.parseMention(global.wm), forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: randomChannel.id, serverMessageId: '', newsletterName: randomChannel.nombre }, externalAdReply: { title: global.wm, body: global.dev, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: global.icons, thumbnail: global.icons, sourceUrl: global.fakeLink }}}, { quoted: m }
+global.fake = { contextInfo: { mentionedJid: conn.parseMention(global.wm), forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363302120826314@newsletter', serverMessageId: '', newsletterName: global.wm }, externalAdReply: { title: global.wm, body: global.dev, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: global.icons, thumbnail: global.icons, sourceUrl: global.fakeLink }}}, { quoted: m }
 }
 export default handler
