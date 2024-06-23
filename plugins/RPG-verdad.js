8763bc9e267ef23201d203f9d2f2151d70f5c64d
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-22 21:13:43 -0400

    Update RPG-verdad.js

--- plugins/RPG-verdad.js
+++ plugins/RPG-verdad.js
@@ -1,5 +1,5 @@
 const handler = async (m, {conn}) => {
-  if (!global.db.data.chats[m.chat].game) return m.reply('✧ Los comandos de RPG han sido desactivados en este grupo.');
+  if (!global.db.data.chats[m.chat].rpg) return m.reply('✧ Los comandos de RPG han sido desactivados en este grupo.');
   conn.reply(m.chat, `❀ *RESPONDE CON LA VERDAD*\n> ✰ *${pickRandom(global.verdad)}*`, m, fake, );
 };
 handler.help = ['verdad'];
