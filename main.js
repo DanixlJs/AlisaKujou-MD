fdedcc87d0b1cbcf1bf3c455dda0e94a94d17782
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-23 18:41:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -48,7 +48,6 @@ global.videoList = [];
 global.videoListXXX = [];
 const __dirname = global.__dirname(import.meta.url);
 global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-global.prefix = ['/'];
 global.db = new Low(/https?:\/\//.test(opts['db'] || '') ? new cloudDBAdapter(opts['db']) : new JSONFile(`${opts._[0] ? opts._[0] + '_' : ''}database.json`));
 global.DATABASE = global.db; 
 global.loadDatabase = async function loadDatabase() {
