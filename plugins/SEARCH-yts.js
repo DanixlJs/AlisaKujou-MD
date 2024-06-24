d054858ff662d247262f1bf1c8c8893372063916
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-24 17:50:52 +0200

    Actualizar SEARCH-yts.js

--- plugins/SEARCH-yts.js
+++ plugins/SEARCH-yts.js
@@ -1,17 +1,17 @@
 import yts from 'yt-search'
 var handler = async (m, { text, conn, args, command, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `✧ Ingresa un texto para realizar la búsqueda, Ejemplo:\n> *${usedPrefix + command} Empanada Durmiendo*`, m, )
+if (!text) return conn.reply(m.chat, `✧ Ingresa un texto para realizar la búsqueda.`, m, )
 let results = await yts(text)
 let tes = results.all
+let txt = `❀ *SEARCH - YOUTUBE*\n`
 let teks = results.all.map(v => {
 switch (v.type) {
-case 'video': return `❀ *YOUTUBE SEARCH*
-✰ *Título ⪼* ${v.title}
+case 'video': return `✰ *Título ⪼* ${v.title}
 ◈ *Link ⪼* ${v.url}
 ◈ *Duración ⪼* ${v.timestamp}
 ◈ *Publicado ⪼* ${v.ago}
 ◈ *Vistas ⪼* ${v.views}`}}).filter(v => v).join('\n\n')
-conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m)
+conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', txt + teks, m)
 }
 handler.help = ['ytsearch <texto>']
 handler.tags = ['search']
