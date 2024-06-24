f89f00c2bc8d8ae43aec0c74b90a4e85d910522b
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-24 18:05:28 +0200

    Update DOWNLOADER-ytmp4.js

--- plugins/DOWNLOADER-ytmp4.js
+++ plugins/DOWNLOADER-ytmp4.js
@@ -5,7 +5,7 @@ if (!args[0].match(/youtu/gi)) return conn.reply(m.chat, `✧ El enlace no es un
 try {
 let { title, size, quality, thumbnail, dl_url } = await Scraper.ytmp4(args[0])
 if (size.includes('GB') || size.replace(' MB', '') > 300) { return await m.reply('✧ El archivo supera los 300MB, se canceló la descarga.')}
-let txt = `❀ *DOWNLOADER - YOUTUBE*\n✰ *Título ⪼* ${title}\n◈ *Cálidad ⪼* ${quality}\n◈ *Peso ⪼* ${size}`
+let txt = `❀ *DOWNLOADER - YOUTUBE*\n✰ *Título ⪼* ${title}\n◈ *Cálidad ⪼* ${quality}\n◈ *Peso ⪼* ${size}> Descargando su archivo, espere un momento.`
 await conn.sendFile(m.chat, thumbnail, 'icono.jpg', txt, m)
 await conn.sendFile(m.chat, dl_url, title + '.mp4', `❀ *Titulo ⪼* ${title}\n◈ *Calidad ⪼* ${quality}`, m, false, { asDocument: true })
 } catch (e){
