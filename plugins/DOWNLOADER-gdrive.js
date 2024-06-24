c2124fece205132d0095e886521bf56dc990066e
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-24 18:39:23 +0200

    Update DOWNLOADER-gdrive.js

--- plugins/DOWNLOADER-gdrive.js
+++ plugins/DOWNLOADER-gdrive.js
@@ -1,45 +1,19 @@
-import fetch from 'node-fetch';
-import {sizeFormatter} from 'human-readable';
-const formatSize = sizeFormatter({
-  std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B`});
-const handler = async (m, {conn, args, usedPrefix }) => {
-  if (!args[0]) await m.reply('✧ Ingrese el Link del archivo que quiera descargar.');
-  try {
-    GDriveDl(args[0]).then(async (res) => {
-      conn.reply(m.chat, '❀ Procesando, espere un momento.', m);
-      if (!res) throw res;
-      conn.sendFile(m.chat, res.downloadUrl, res.fileName, '', m, null, {mimetype: res.mimetype, asDocument: true});
-    });
-  } catch (e) {
-    m.reply('✧ Ocurrió un error inesperado.');
-    console.log(e);
-  }
-};
-handler.command = ['gdrive'];
-handler.registrado = true;
-handler.premium = true;
-handler.tags = ['downloader', 'premium'];
-handler.help = ['gdrive <link>'];
-export default handler;
-async function GDriveDl(url) {
-  let id;
-  if (!(url && url.match(/drive\.google/i))) throw 'Invalid URL';
-  id = (url.match(/\/?id=(.+)/i) || url.match(/\/d\/(.*?)\//))[1];
-  if (!id) throw 'ID Not Found';
-  const res = await fetch(`https://drive.google.com/uc?id=${id}&authuser=0&export=download`, {
-    method: 'post',
-    headers: {
-      'accept-encoding': 'gzip, deflate, br',
-      'content-length': 0,
-      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-      'origin': 'https://drive.google.com',
-      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-      'x-client-data': 'CKG1yQEIkbbJAQiitskBCMS2yQEIqZ3KAQioo8oBGLeYygE=',
-      'x-drive-first-party': 'DriveWebUi',
-      'x-json-requested': 'true'}});
-  const {fileName, sizeBytes, downloadUrl} = JSON.parse((await res.text()).slice(4));
-  if (!downloadUrl) throw 'Link Download Limit!';
-  const data = await fetch(downloadUrl);
-  if (data.status !== 200) throw data.statusText;
-  return {downloadUrl, fileName, fileSize: formatSize(sizeBytes), mimetype: data.headers.get('content-type')};
-}
+import Scraper from '@SumiFX/Scraper'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return m.reply('✧ Ingresa un enlace de un archivo de Google Drive.')
+let user = global.db.data.users[m.sender]
+try {
+let { title, size, type, dl_url } = await Scraper.GDriveDl(args[0])
+if (size.includes('GB') || size.replace(' MB', '') > user.premium ? 500 : 100) { return await m.reply(`El archivo pesa mas de ${user.premium ? '500' : '100'}MB, ${user.premium ? 'se canceló la descarga.' : 'pasate premium para descargar archivos de hasta 500MB.'}`)}
+let txt = `❀ *DOWNLOADER - GDRIVE*\n✰ *Nombre * ${title}\n◈ *Peso * ${size}\n> Descargando su archivo, espere un momento.`
+await conn.reply(m.chat, txt, m, fake, )
+await conn.sendMessage(m.chat, { document: { url: dl_url }, fileName: title, mimetype: type }, { quoted: m })
+} catch (e){
+m.reply('✧ Ocurrió un error inesperado.')
+console.log(e)
+}}
+handler.help = ['gdrive <enlace>']
+handler.tags = ['downloader', 'premium']
+handler.command = ['gdrive']
+handler.registrado = true
+export default handler
