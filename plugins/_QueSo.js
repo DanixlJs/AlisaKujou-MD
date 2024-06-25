816241d1090ebc8b4e21c1c4a4acc8431b0257b5
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 14:08:11 -0500

    Actualizar _QueSo.js

--- plugins/_QueSo.js
+++ plugins/_QueSo.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command, __dirname }) => {
 try {
 let info = `*So* 🧀`
@@ -15,4 +15,4 @@ mediaType: 2,
 console.error(error)}}
 handler.customPrefix = /^(Que|que|q|Q|k)$/i
 handler.command = new RegExp
-export default handler
+export default handler*/
