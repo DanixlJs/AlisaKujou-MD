d0b85e46519b14ab1fcbad2cd461c6733ed7b704
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 14:11:45 -0500

    Actualizar _random.js

--- plugins/_random.js
+++ plugins/_random.js
@@ -1,17 +1,17 @@
 import fs from 'fs';
 const handler = (m) => m;
 handler.all = async function(m) {
-const fkontak = { contextInfo: { mentionedJid: conn.parseMention(global.wm), forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363302120826314@newsletter', serverMessageId: '', newsletterName: global.botname }, externalAdReply: { title: global.wm + '\n' + global.dev, body: '❀ Hola ' + nametag, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: global.icons, thumbnail: global.icons, sourceUrl: channel }}}, { quoted: m }
+
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
 conn.reply(m.chat, `❀ ¡Hola! Soy ${global.wm}, en que puedo ayudarte hoy?\n✰ Usa */menu* para ver mis comandos.`, m, fake, )
 }
 if (/^que|Que|q|Q$/i.test(m.text)) {
-conn.reply(m.chat, `so 🧀`, m, fkontak, )
+conn.reply(m.chat, `so 🧀`, m, fake, )
 }
 if (/^a|A$/i.test(m.text)) {
-conn.reply(m.chat, `*rrocito* 🍚`, m, fkontak, )
+conn.reply(m.chat, `*rrocito* 🍚`, m, fake, )
 }
 return !0;
 };
