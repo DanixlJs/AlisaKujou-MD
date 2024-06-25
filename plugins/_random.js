8e40104594caa45d257eefadac94435520f97c5c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 14:07:26 -0500

    Actualizar _random.js

--- plugins/_random.js
+++ plugins/_random.js
@@ -6,11 +6,11 @@ if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
 conn.reply(m.chat, `❀ ¡Hola! Soy ${global.wm}, en que puedo ayudarte hoy?\n✰ Usa */menu* para ver mis comandos.`, m, fake, )
 }
-if (/^que$/i.test(m.text)) {
-conn.reply(m.chat, 'so 🧀', m, fake, )
+if (/^queso$/i.test(m.text)) {
+conn.reply(m.chat, `so 🧀`, m, fake, )
 }
 if (/^a\A$/i.test(m.text)) {
-conn.reply(m.chat, '*rrocito* 🍚', m, fake, )
+conn.reply(m.chat, `*rrocito* 🍚`, m, fake, )
 }
 return !0;
 };
