635bf9cc08036904c829a8ed55f1a71638815c25
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 14:00:33 -0500

    Actualizar _QueSo.js

--- plugins/_QueSo.js
+++ plugins/_QueSo.js
@@ -2,34 +2,30 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 let queso = `*so* 🧀`
 let arroz = `*rrocito* 🍚`
 
-if (command == 'que' || command == 'Que' || command == 'q' || command == 'Q') {
-await conn.sendMessage(m.chat, { text: queso,
-contextInfo:{
-forwardingScore: 9999999,
-isForwarded: false, 
-"externalAdReply": {
-"showAdAttribution": true,
-"containsAutoReply": true,
-"title": packname + ' 🤍',
-"previewType": "PHOTO",
-"thumbnailUrl": icons, 
-"sourceUrl": channel}}},
-{ quoted: m})
+if (command == 'que' || command == 'Que' || command == 'q' || command == 'Q') 
+conn.sendMessage(m.chat, {
+text: queso,
+contextInfo: {
+externalAdReply: {
+title: botname + ' 🤍',
+body: botname,
+thumbnailUrl: icons,
+sourceUrl: channel,
+mediaType: 2,
+}}}, { quoted: m })
 }
 
-if (command == 'a' || command == 'A' || command == 'aa' {
- await conn.sendMessage(m.chat, { text: arroz,
-contextInfo:{
-forwardingScore: 9999999,
-isForwarded: false, 
-"externalAdReply": {
-"showAdAttribution": true,
-"containsAutoReply": true,
-"title": packname + ' 🤍',
-"previewType": "PHOTO",
-"thumbnailUrl": icons, 
-"sourceUrl": channel}}},
-{ quoted: m})
+if (command == 'a' || command == 'aa' || command == 'A' || command == 'Aa') 
+conn.sendMessage(m.chat, {
+text: arroz,
+contextInfo: {
+externalAdReply: {
+title: botname + ' 🤍',
+body: botname,
+thumbnailUrl: icons,
+sourceUrl: channel,
+mediaType: 2,
+}}}, { quoted: m });
 }}
 
 handler.command = /^(que|Que|q|Q|A|a|aa)$/i
