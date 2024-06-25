7777fa1107615447ab4f2ac69b84d22b8442c2f9
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 13:35:36 -0500

    Actualizar plugins

---
+++ plugins/_QueSo.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text, usedPrefix, command, __dirname }) => {
+try {
+let info = `So 🧀`
+conn.sendMessage(m.chat, {
+text: info,
+contextInfo: {
+externalAdReply: {
+title: packname,
+thumbnailUrl: icons,
+sourceUrl: channel,
+mediaType: 2,
+}}}, { quoted: m });
+} catch (error) {
+console.error(error)}}
+handler.customPrefix = /^(Que|que|q|Q|k)$/i
+handler.command = new RegExp
+export default handler
