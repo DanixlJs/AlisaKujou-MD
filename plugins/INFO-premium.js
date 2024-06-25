5746c3e809fedaee0bfb817b516bd676da4c6411
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-25 07:20:27 +0200

    Actualizar INFO-premium.js

--- plugins/INFO-premium.js
+++ plugins/INFO-premium.js
@@ -1,8 +1,53 @@
 let handler = async (m, { conn, text, usedPrefix, command, isPrems}) => {
-     if (isPrems) return m.reply(`✧ Ya eres un Usuario Premium.`);
-     let noPrem = `❀ Puedes adquirir tu Pase Premium en nuestro Patreon.\n> ${global.patreon}\n\n✰ Con tu Compra recibiras un Pase Premium y tambien los siguientes Articulos.\n◈ Diamantes\n◈ AlisaCoins\n◈ Experiencia\n◈ Y mas.`;
-     if (!isPrems) conn.reply(m.chat, `${noPrem}`, m, fake, );
+if (isPrems) return m.reply(`✧ Ya eres un Usuario Premium.`);
+let user = global.db.data.users;
+let Shop = `❀ *TIENDA - SHOP*
+✰ Bienvenido a la tienda ヾ(❀╹◡╹)ﾉﾞ
+
+❀ *ARTÍCULOS*
+✰ _Pase Nivel 1 🏷️_
+◈ *Tiempo:* 5 minutos
+◈ *Precio:* 670.000 ${global.botcoins}
+◈ *_${usedPrefix}pass1_*
+
+✰ _Pase Nivel 2 🏷️_
+◈ *Tiempo:* 10 minutos
+◈ *Precio:* 1.000.000 ${global.botcoins}
+◈ *_${usedPrefix}pass2_*
+
+✰ _Pase Nivel 3 🏷️_
+◈ *Tiempo:* 1 hora
+◈ *Precio:* 10.000 Diamantes
+◈ *_${usedPrefix}pass3_*
+
+◈ _Pase Nivel 4 💎_
+◈ *Tiempo:* 1 mes
+◈ *Precio:* 2 USD
+◈ *Tienda:* ${global.patreon}
+`;
+if (command === 'premium' && !isPrems) conn.reply(m.chat, Shop, m, fake, );
+if (command === 'pass1') {
+if (user.dinero < 670000) return m.reply('✧ No tienes suficientes ${global.botcoins} para comprar éste Producto.');
+user.dinero -= 670000;
+user.premiumTime += 300000;
+user.premium = true;
+conn.reply(m.chat, '❀ Felicidades, ahora eres un usuario premium por 5 minutos.', m, fake, );
+};
+if (command === 'pass2') {
+if (user.dinero < 1000000) return m.reply('✧ No tienes suficientes ${global.botcoins} para comprar éste Producto.');
+user.dinero -= 1000000;
+user.premiumTime += 600000;
+user.premium = true;
+conn.reply(m.chat, '❀ Felicidades, ahora eres un usuario premium por 10 minutos.', m, fake, );
+};
+if (command === 'pass3') {
+if (user.diamantes < 10000) return m.reply('✧ No tienes suficientes Diamantes para comprar éste Producto.');
+user.diamantes -= 10000;
+user.premiumTime += 3600000;
+user.premium = true;
+conn.reply(m.chat, '❀ Felicidades, ahora eres un usuario premium por 1 hora.', m, fake, );
 }
+};
 handler.registrado = true;
 handler.help = ['premium'];
 handler.command = ['premium'];
