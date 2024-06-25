7546b19b1ca2166a323fc7965ecd907d76d1821e
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 13:55:08 -0500

    Actualizar _QueSo.js

--- plugins/_QueSo.js
+++ plugins/_QueSo.js
@@ -2,28 +2,35 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 let queso = `*so* 🧀`
 let arroz = `*rrocito* 🍚`
 
-if (command == 'que' || command == 'Que' || command == 'q' || command == 'Q') 
-conn.sendMessage(m.chat, {
-text: queso,
+if (command == 'que' || command == 'Que' || command == 'q' || command == 'Q') {
+'', 'queso.jpg', queso.trim(), fkontak, true, {
 contextInfo: {
+'forwardingScore': 200,
+'isForwarded': false,
 externalAdReply: {
-title: botname + ' 🤍',
-thumbnailUrl: 'https://qu.ax/zPKl.jpg',
+showAdAttribution: true,
+renderLargerThumbnail: false,
+title: packname + ' 🤍',
+mediaType: 1,
 sourceUrl: channel,
-mediaType: 2,
-}}}, { quoted: m })
+thumbnailUrl: icons
+}}
+}, { mentions: m.sender })
 }
 
-if (command == 'a' || command == 'aa' || command == 'A' || command == 'Aa') 
-conn.sendMessage(m.chat, {
-text: arroz,
-contextInfo: {
-externalAdReply: {
-title: botname + ' 🤍',
-thumbnailUrl: 'https://qu.ax/zPKl.jpg',
-sourceUrl: channel,
-mediaType: 2,
-}}}, { quoted: m });
+if (command == 'a' || command == 'A' || command == 'aa' {
+ await conn.sendMessage(m.chat, { text: txt,
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: false, 
+"externalAdReply": {
+"showAdAttribution": true,
+"containsAutoReply": true,
+title: packname + ' 🤍',
+"previewType": "PHOTO",
+thumbnailUrl: icon, 
+sourceUrl: channel}}},
+{ quoted: fkontak})
 }}
 
 handler.command = /^(que|Que|q|Q|A|a|aa)$/i
