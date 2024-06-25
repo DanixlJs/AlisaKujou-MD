ad34f7ff18170bb4ba143a095acd183811f8ee37
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-25 03:15:32 +0200

    Actualizar DOWNLOADER-spotify.js

--- plugins/DOWNLOADER-spotify.js
+++ plugins/DOWNLOADER-spotify.js
@@ -3,7 +3,7 @@ const handler = async (m, { conn, text }) => {
 if (!text) return m.reply(`✧ Ingresa el nombre de la canción que quieras descargar.`)
 try {
 let { title, artist, album, published, thumbnail, dl_url } = await Scraper.spotify(text)
-let txt = `❀ *SPOTIFY - SEARCH*\n✰ *Nombre ⪼* ${title}\n◈ *Artista ⪼* ${artist}\n◈ *Álbum ⪼* ${album}\n◈ *Publicado ⪼* ${published}\n> Descargando su canción, espere un momento.`
+let txt = `❀ *SPOTIFY - SEARCH*\n✰ *Nombre ⪼* ${title}\n◈ *Artista ⪼* ${artist}\n◈ *Álbum ⪼* ${album}\n◈ *Publicado ⪼* ${published}\n\n> Descargando su canción, espere un momento.`
 await conn.sendFile(m.chat, thumbnail, 'icon.jpg', txt, m)
 await conn.sendFile(m.chat, dl_url, title + '.mp3', `✰ *Titulo ⪼* ${title}\n◈ *Artista ⪼* ${artist}`, m, )
 } catch (e){
