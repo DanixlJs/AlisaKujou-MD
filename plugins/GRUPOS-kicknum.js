1599fc92563feb040248874765b6fb237689d519
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-25 23:34:50 +0200

    Update GRUPOS-kicknum.js

--- plugins/GRUPOS-kicknum.js
+++ plugins/GRUPOS-kicknum.js
@@ -1,41 +1,32 @@
 const handler = async (m, { conn, args, participants, usedPrefix, command }) => {
     const prefix = args[0];
     if (!prefix) return m.reply(`✧ Ingresa un prefijo de país, Ejemplo:\n> *${usedPrefix + command} 92*`);
-
     const membersWithPrefix = participants
         .filter(p => p.id.startsWith(prefix))
         .map(p => p.id);
-
     if (command === 'listnum') {
         if (membersWithPrefix.length === 0) {
             return conn.reply(m.chat, `✧ No se encontraron miembros con el prefijo *+${prefix}*.`, m);
         }
-
         const memberList = membersWithPrefix.map(id => `> → ${id.replace('@s.whatsapp.net', '').trim()}`).join('\n');
         return conn.reply(m.chat, `❀ *RESULTADOS*\n✰ *Prefijo ⪼* +${prefix}:\n${memberList}`, m, fake, );
     }
-
     if (command === 'kicknum') {
         if (membersWithPrefix.length === 0) {
             return conn.reply(m.chat, `✧ No se encontraron miembros con el prefijo *+${prefix}* para expulsar.`, m);
         }
-
         const kickMember = async (index) => {
             if (index >= membersWithPrefix.length) return;
-
             await conn.groupParticipantsUpdate(m.chat, [membersWithPrefix[index]], 'remove');
             setTimeout(() => kickMember(index + 1), 2000); 
         };
-
         await conn.reply(m.chat, `❀ Comenzando eliminación.`, m);
         kickMember(0);
     }
 };
-
 handler.help = ['listnum <prefijo>', 'kicknum <prefijo>'];
 handler.tags = ['grupo'];
 handler.command = ['listnum', 'kicknum'];
 handler.admin = true;
 handler.botAdmin = true;
-
 export default handler;
