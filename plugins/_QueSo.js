18a29b40c26a82ba8401a2316200fcb59e16c775
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 14:04:52 -0500

    Actualizar _QueSo.js

--- plugins/_QueSo.js
+++ plugins/_QueSo.js
@@ -1,40 +1,18 @@
-let handler = async (m, { conn, command, usedPrefix }) => {
-
-let queso = `*so* 🧀`
-let txt = `*rrocito* 🍚`
-
-if (command == 'que') {
-await conn.sendFile(m.chat, '', 'cafi.jpg', queso.trim(), fkontak, true, {
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text, usedPrefix, command, __dirname }) => {
+try {
+let info = `*So* 🧀`
+conn.sendMessage(m.chat, {
+text: info,
 contextInfo: {
-'forwardingScore': 200,
-'isForwarded': false,
 externalAdReply: {
-showAdAttribution: true,
-renderLargerThumbnail: false,
-title: botname,
-body: `🤍 Botsito`,
-mediaType: 1,
+title: botname + ' 🤍',
+thumbnailUrl: 'https://qu.ax/zPKl.jpg',
 sourceUrl: channel,
-thumbnailUrl: icons
-}}
-}, { mentions: m.sender })
-}
-
-if (command == 'a' || command == 'aa' || command == 'A' || command == 'Aa') {
- await conn.sendMessage(m.chat, { text: txt,
-contextInfo:{
-forwardingScore: 9999999,
-isForwarded: false, 
-"externalAdReply": {
-"showAdAttribution": true,
-"containsAutoReply": true,
-title: botname,
-body: `🤍 Botsito`,
-"previewType": "PHOTO",
-thumbnailUrl: icons, 
-sourceUrl: channel}}},
-{ quoted: fkontak})
-}}
-
-handler.command = /^(que|aa|Aa|A|a)$/i
+mediaType: 2,
+}}}, { quoted: m });
+} catch (error) {
+console.error(error)}}
+handler.customPrefix = /^(Que|que|q|Q|k)$/i
+handler.command = new RegExp
 export default handler
