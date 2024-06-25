495e929075b2c73e65fd166f57de4f75b6d6d081
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-25 05:00:32 +0200

    Update DOWNLOADER-gdrive.js

--- plugins/DOWNLOADER-gdrive.js
+++ plugins/DOWNLOADER-gdrive.js
@@ -1,19 +1,25 @@
-import Scraper from '@SumiFX/Scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply('✧ Ingresa un enlace de un archivo de Google Drive.')
-let user = global.db.data.users[m.sender]
-try {
-let { title, size, type, dl_url } = await Scraper.GDriveDl(args[0])
-if (size.includes('GB') || size.replace(' MB', '') > user.premium ? 500 : 100) { return await m.reply(`El archivo pesa mas de ${user.premium ? '500' : '100'}MB, ${user.premium ? 'se canceló la descarga.' : 'pasate premium para descargar archivos de hasta 500MB.'}`)}
-let txt = `❀ *DOWNLOADER - GDRIVE*\n✰ *Nombre * ${title}\n◈ *Peso * ${size}\n\n> Descargando su archivo, espere un momento.`
-await conn.reply(m.chat, txt, m, fake, )
-await conn.sendMessage(m.chat, { document: { url: dl_url }, fileName: title, mimetype: type }, { quoted: m })
-} catch (e){
-m.reply('✧ Ocurrió un error inesperado.')
-console.log(e)
-}}
-handler.help = ['gdrive <enlace>']
-handler.tags = ['downloader', 'premium']
-handler.command = ['gdrive']
-handler.registrado = true
-export default handler
+import Scraper from '@SumiFX/Scraper';
+let handler = async (m, { conn, args }) => {
+    if (!args[0]) return m.reply('✧ Ingresa un enlace de un archivo de Google Drive.');
+    let user = global.db.data.users[m.sender];
+    try {
+        let { title, size, type, dl_url } = await Scraper.GDriveDl(args[0]);
+        let fileSize = parseFloat(size.replace(/[^0-9.]/g, ''));
+        let isGB = size.includes('GB');
+        if (isGB) fileSize *= 1024; 
+        if (fileSize > (user.premium ? 500 : 100)) {
+            return await m.reply(El archivo pesa más de ${user.premium ? '500' : '100'}MB, ${user.premium ? 'se canceló la descarga.' : 'pásate a premium para descargar archivos de hasta 500MB.'});
+        }
+        let txt = ❀ *DOWNLOADER - GDRIVE*\n✰ *Nombre:* ${title}\n◈ *Peso:* ${size}\n\n> Descargando su archivo, espere un momento.;
+        await conn.reply(m.chat, txt, m);
+        await conn.sendMessage(m.chat, { document: { url: dl_url }, fileName: title, mimetype: type }, { quoted: m });
+    } catch (e) {
+        m.reply('✧ Ocurrió un error inesperado.');
+        console.log(e);
+    }
+};
+handler.help = ['gdrive <enlace>'];
+handler.tags = ['downloader', 'premium'];
+handler.command = ['gdrive'];
+handler.registrado = true;
+export default handler;
