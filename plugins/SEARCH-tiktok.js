4d064997339adcfa47c6d8722aab182237f251b6
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 16:11:49 -0500

    Actualizar SEARCH-tiktok.js

--- plugins/SEARCH-tiktok.js
+++ plugins/SEARCH-tiktok.js
@@ -1,43 +1,5 @@
-import axios from 'axios'
-import Starlights from '@StarlightsTeam/Scraper'
-
-let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  if (!text) return conn.reply(m.chat, `💙 Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* Alisa Kujou Edit`, m, fake)
-
-  await m.react('🕓')
-  let img = await (await axios.get('https://i.ibb.co/kyTcqt9/file.jpg', { responseType: 'arraybuffer' })).data
-
-  try {
-    let data = await Starlights.tiktokSearch(text)
-
-    if (data && data.length > 0) {
-      let txt = `*乂  T I K T O K  -  S E A R C H*`
-      for (let i = 0; i < (50 <= data.length ? 50 : data.length); i++) {
-        let video = data[i]
-        txt += `\n\n`
-        txt += `  *» Nro* : ${i + 1}\n`
-        txt += `  *» Título* : ${video.title}\n`
-        txt += `  *» Autor* : ${video.author}\n`
-        txt += `  *» Url* : ${video.url}`
-      }
-      await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt)
-      await m.react('✅')
-    } else {
-      await conn.react('✖️')
-    }
-  } catch {
-    await m.react('✖️')
-  }
-}
-handler.tags = ['search']
-handler.help = ['tiktoksearch *<búsqueda>*']
-handler.command = ['tiktoksearch', 'tiktoks']
-handler.register = true
-
-export default handler
-
-/*import axios from 'axios'
-import Starlights from '@StarlightsTeam/Scraper'
+import axios from 'axios';
+const { proto, generateWAMessageFromContent, generateWAMessageContent } = (await import('@whiskeysockets/baileys')).default;
 
 let handler = async (m, { conn, text }) => {
 if (!text) return conn.reply(m.chat, '💥 Ingresa un texto para realizar la búsqueda.', m, fake);
@@ -55,7 +17,7 @@ const shuffleArray = (array) => {
 };
 
 try {
-    const { data } = await axios.get(`https://apis-starlights-team-cbb6f3a3.koyeb.app/starlight/tiktoksearch?text=${text}`);
+let data = await Starlights.tiktokSearch(text)
     const results = data.data;
 
     if (!results.length) return conn.reply(m.chat, '💌 No se encontraron resultados.', m);
@@ -66,7 +28,7 @@ try {
 
     const push = await Promise.all(selectedResults.map(async (result) => ({
         body: { text: null },
-        footer: { text: `${global.dev}` },
+        footer: { text: `💥 Ofc Daniel` },
         header: {
             title: result.title,
             hasMediaAttachment: true,
@@ -98,30 +60,7 @@ try {
 
 handler.command = ['tiktoksearch'];
 handler.help = ['tiktoksearch <texto>'];
-//handler.register = true;
+handler.register = true;
 handler.tags = ['search'];
 
-export default handler;*/
-
-
-/*import axios from 'axios';
-let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '✧ Ingresa un texto para realizar la búsqueda.', m);
-const getRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
-try {
-    const { data } = await axios.get(`https://apis-starlights-team-cbb6f3a3.koyeb.app/starlight/tiktoksearch?text=${text}`);
-    const results = data.data;
-    if (!results.length) return conn.reply(m.chat, '✧ No se encontraron resultados.', m);
-    const randomResult = getRandomElement(results);
-    const videoUrl = randomResult.nowm;
-    await conn.sendFile(m.chat, videoUrl, 'tts.mp4', `❀ *TIKTOK SEARCH*\n✰ *Resultados para:*\n> ${text}`, m);
-} catch (error) {
-    console.log(error);
-}
-};
-
-handler.command = ['tts', 'tiktokvid', 'tiktoksearch'];
-handler.help = ['tiktoksearch <texto>'];
-handler.registrado = true;
-handler.diamantes = 1;
-export default handler;*/
+export default handler;
