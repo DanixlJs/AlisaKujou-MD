c080d3fb2f7cca918b5cb1b928d6e82886de23da
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-25 03:11:44 +0200

    Actualizar DOWNLOADER-gdrive.js

--- plugins/DOWNLOADER-gdrive.js
+++ plugins/DOWNLOADER-gdrive.js
@@ -5,7 +5,7 @@ let user = global.db.data.users[m.sender]
 try {
 let { title, size, type, dl_url } = await Scraper.GDriveDl(args[0])
 if (size.includes('GB') || size.replace(' MB', '') > user.premium ? 500 : 100) { return await m.reply(`El archivo pesa mas de ${user.premium ? '500' : '100'}MB, ${user.premium ? 'se canceló la descarga.' : 'pasate premium para descargar archivos de hasta 500MB.'}`)}
-let txt = `❀ *DOWNLOADER - GDRIVE*\n✰ *Nombre * ${title}\n◈ *Peso * ${size}\n> Descargando su archivo, espere un momento.`
+let txt = `❀ *DOWNLOADER - GDRIVE*\n✰ *Nombre * ${title}\n◈ *Peso * ${size}\n\n> Descargando su archivo, espere un momento.`
 await conn.reply(m.chat, txt, m, fake, )
 await conn.sendMessage(m.chat, { document: { url: dl_url }, fileName: title, mimetype: type }, { quoted: m })
 } catch (e){
