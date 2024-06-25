bf22c8e76b94d610a2148d476ed6a4673a0567c1
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-25 03:15:16 +0200

    Actualizar DOWNLOADER-play.js

--- plugins/DOWNLOADER-play.js
+++ plugins/DOWNLOADER-play.js
@@ -5,7 +5,7 @@ try {
 let res = await Scraper.ytsearch(text)
 let { title, size, quality, thumbnail, dl_url } = await Scraper.ytmp3(res[0].url)
 if (size.includes('GB') || size.replace(' MB', '') > 100) { return await m.reply('✧ El archivo supera los 100 MB, se canceló la descarga.')}
-let txt = `❀ *DOWNLOADER - YOUTUBE*\n✰ *Título ⪼* ${title}\n◈ *Duración ⪼* ${res[0].duration}\n◈ *Autor ⪼* ${res[0].author}\n◈ *Calidad ⪼* ${quality}\n◈ *Publicado ⪼* ${res[0].published}\n◈ *Peso ⪼* ${size}\n◈ *Url ⪼* ${res[0].url}> Descargando su canción, espere un momento.`
+let txt = `❀ *DOWNLOADER - YOUTUBE*\n✰ *Título ⪼* ${title}\n◈ *Duración ⪼* ${res[0].duration}\n◈ *Autor ⪼* ${res[0].author}\n◈ *Calidad ⪼* ${quality}\n◈ *Publicado ⪼* ${res[0].published}\n◈ *Peso ⪼* ${size}\n◈ *Url ⪼* ${res[0].url}\n\n> Descargando su canción, espere un momento.`
 await conn.sendFile(m.chat, thumbnail, 'icon.jpg', txt, m)
 await conn.sendFile(m.chat, dl_url, title + '.mp3', `❀ *Titulo ⪼* ${title}\n◈ *Calidad ⪼* ${quality}`, m, false, { mimetype: 'audio/mpeg', asDocument: true})
 } catch (e){
