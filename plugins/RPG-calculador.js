14e51b092ece4e0d0b24b8892149326b70a8ecca
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 15:54:07 -0500

    Actualizar RPG-calculador.js

--- plugins/RPG-calculador.js
+++ plugins/RPG-calculador.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!global.db.data.chats[m.chat].rpg) return m.reply('✧ Los comandos de RPG han sido desactivados en este grupo.');
-  return conn.reply(m.chat, '✧ Etiqueta al usuario.', m);
+  if (!text) return conn.reply(m.chat, `✧ Mensiona a un Usuario.`, m, fake, );
   const percentages = (500).getRandom();
   let emoji = '';
   let description = '';
