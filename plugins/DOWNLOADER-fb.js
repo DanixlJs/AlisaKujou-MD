911ed3d15aa06393a0725a98b1c180395a562609
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-26 04:57:10 +0200

    Update DOWNLOADER-fb.js

--- plugins/DOWNLOADER-fb.js
+++ plugins/DOWNLOADER-fb.js
@@ -1,8 +1,8 @@
-import Scraper from '@SumiFX/Scraper'
+import Starlights from '@StarlightsTeam/Scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) return m.reply('✧ Ingresa un enlace de Facebook.')
 try {
-let { title, SD, HD } = await Scraper.fbdl(args[0])
+let { title, SD, HD } = await Starlights.fbdl(args[0])
 await conn.sendMessage(m.chat, { video: { url: SD || HD }, caption: `❀ *DOWNLOADER - FACEBOOK*\n✰ *Titulo ⪼* ${title}` }, { quoted: m})
 } catch (e){
 m.reply('Ocurrió un  error inesperado.')
