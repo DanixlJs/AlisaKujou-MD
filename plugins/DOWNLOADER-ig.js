1dc035c54b95d169819c005aaa9adc31d3494354
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-26 04:50:10 +0200

    Update DOWNLOADER-ig.js

--- plugins/DOWNLOADER-ig.js
+++ plugins/DOWNLOADER-ig.js
@@ -1,8 +1,8 @@
-import Scraper from '@SumiFX/Scraper'
+import Starlights from '@StarlightsTeam/Scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) return m.reply('✧ Ingresa un enlace de Instagram.')
 try {
-let { dl_url } = await Scraper.igdl(args[0])
+let { dl_url } = await Starlights.igdl(args[0])
 await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: '❀ Aqui tiene.' }, { quoted: m})
 } catch (e) {
   console.log(e)
