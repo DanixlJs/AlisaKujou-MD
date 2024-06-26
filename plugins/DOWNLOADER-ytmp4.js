f0dc7cd04175df4492c1ec3db7269d0c57d8dfe6
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-26 06:01:10 +0200

    Update DOWNLOADER-ytmp4.js

--- plugins/DOWNLOADER-ytmp4.js
+++ plugins/DOWNLOADER-ytmp4.js
@@ -8,10 +8,10 @@ let { title, size, quality, thumbnail, dl_url } = await Scraper.ytmp4(args[0])
 let fileSize = parseFloat(size.replace(/[^0-9.]/g, ''))
 let isGB = size.includes('GB')
 if (isGB) fileSize *= 1024
-if (fileSize > (user.premium ? 500 : 100)) { return await m.reply(`✧ El archivo supera los ${user.premium ? 500 : 100}MB, ${user.premium ? 'se canceló la descarga.' : 'pasate a premium para descargar archivos de hasta 500MB.'}`)}
+if (fileSize > (user.premium ? 500 : 200)) { return await m.reply(`✧ El archivo supera los ${user.premium ? 500 : 200}MB, ${user.premium ? 'se canceló la descarga.' : 'pasate a premium para descargar archivos de hasta 500MB.'}`)}
 let txt = `❀ *DOWNLOADER - YOUTUBE*\n✰ *Título ⪼* ${title}\n◈ *Cálidad ⪼* ${quality}\n◈ *Peso ⪼* ${size}\n\n> Descargando su archivo, espere un momento.`
 await conn.sendFile(m.chat, thumbnail, 'icono.jpg', txt, m)
-await conn.sendFile(m.chat, dl_url, title + '.mp4', `❀ *Titulo ⪼* ${title}\n◈ *Calidad ⪼* ${quality}`, m, false, { asDocument: true })
+await conn.sendFile(m.chat, dl_url, title + '.mp4', `❀ *Titulo ⪼* ${title}\n◈ *Calidad ⪼* ${quality}`, m, false, { asDocument: fileSize > 100 })
 } catch (e){
 m.reply('✧ Ocurrió un error inesperado.')
 console.log(e)
