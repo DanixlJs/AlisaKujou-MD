762cf4372d80afef15e734bc2abe805c2a88b622
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-26 06:05:36 +0200

    Update DOWNLOADER-ytmp3.js

--- plugins/DOWNLOADER-ytmp3.js
+++ plugins/DOWNLOADER-ytmp3.js
@@ -8,10 +8,10 @@ let { title, size, quality, thumbnail, dl_url } = await Scraper.ytmp3(args[0])
 let fileSize = parseFloat(size.replace(/[^0-9.]/g, ''))
 let isGB = size.includes('GB')
 if (isGB) fileSize *= 1024
-if (fileSize > (user.premium ? 500 : 100)) { return await m.reply(`✧ El archivo supera los ${user.premium ? 500 : 100}MB, ${user.premium ? 'se canceló la descarga.' : 'pasate a premium para descargar archivos de hasta 500MB.'}`)}
+if (fileSize > (user.premium ? 300 : 100)) { return await m.reply(`✧ El archivo supera los ${user.premium ? 300 : 100}MB, ${user.premium ? 'se canceló la descarga.' : 'pasate a premium para descargar archivos de hasta 300MB.'}`)}
 let txt = `❀ *DOWNLOADER - YOUTUBE*\n✰ *Título ⪼* ${title}\n◈ *Cálidad ⪼* ${quality}\n◈ *Peso ⪼* ${size}\n\n> Descargando su archivo, espere un momento.`
 await conn.sendFile(m.chat, thumbnail, 'icono.jpg', txt, m)
-await conn.sendFile(m.chat, dl_url, title + '.mp3', `❀ *Titulo ⪼* ${title}\n◈ *Calidad ⪼* ${quality}`, m, false, { mimetype: 'audio/mpeg', asDocument: true })
+await conn.sendFile(m.chat, dl_url, title + '.mp3', `❀ *Titulo ⪼* ${title}\n◈ *Calidad ⪼* ${quality}`, m, false, { mimetype: 'audio/mpeg', asDocument: fileSize > 50 })
 } catch (e){
 m.reply('✧ Ocurrió un error inesperado.')
 console.log(e)
