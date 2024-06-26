4fabb5c3fc85eba37bbe86018b49b2c14710bb5b
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-26 04:34:03 +0200

    Update SEARCH-tiktok.js

--- plugins/SEARCH-tiktok.js
+++ plugins/SEARCH-tiktok.js
@@ -1,66 +1,14 @@
-import axios from 'axios';
-const { proto, generateWAMessageFromContent, generateWAMessageContent } = (await import('@whiskeysockets/baileys')).default;
-
-let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '💥 Ingresa un texto para realizar la búsqueda.', m, fake);
-
-const createVideo = async (url) => {
-    const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer });
-    return videoMessage;
-};
-
-const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-};
-
-try {
-    const { data } = await axios.get(`https://delirius-api-oficial.vercel.app/api/tiktoksearch?query=${encodeURIComponent(text)}`);
-    const results = data.data;
-
-    if (!results.length) return conn.reply(m.chat, '💌 No se encontraron resultados.', m);
-
-    shuffleArray(results);
-
-    const selectedResults = results.slice(0, 7);
-
-    const push = await Promise.all(selectedResults.map(async (result) => ({
-        body: { text: null },
-        footer: { text: `💥 Ofc Daniel` },
-        header: {
-            title: result.title,
-            hasMediaAttachment: true,
-            videoMessage: await createVideo(result.nowm)
-        },
-        nativeFlowMessage: { buttons: [] }
-    })));
-
-    const msgContent = {
-        viewOnceMessage: {
-            message: {
-                messageContextInfo: { deviceListMetadata: {}, deviceListMetadataVersion: 2 },
-                interactiveMessage: {
-                    body: { text: '💥 *TIKTOK SEARCH* 💥' },
-                    footer: { text: null },
-                    header: { hasMediaAttachment: false },
-                    carouselMessage: { cards: push }
-                }
-            }
-        }
-    };
-
-    const msg = generateWAMessageFromContent(m.chat, msgContent, {});
-    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
-} catch (error) {
-    console.log(error);
+import Starlights from '@StarlightsTeam/Scraper'
+let handler = async (m, {conn, text}) => {
+    try {
+    if (!text) return m.reply('Ingresa un texto para realizar la busqueda.')
+    await m.react('🕐')
+    let {dl_url, author, likes} = await Starlights.tiktokvid(text)
+    let txt = `*SEARCH - TIKTOK*\n *Titulo * ${text}\n *Autor * ${author}\n *Likes * ${likes}`
+    conn.sendFile(m.chat, dl_url, text + '.mp4', txt, m, fake, )
+    m.react('☑️')
+} catch (e){
+        m.reply('Ocurrió un error inesperado.')
+        console.log(e)
 }
-};
-
-handler.command = ['tiktoksearch'];
-handler.help = ['tiktoksearch <texto>'];
-handler.register = true;
-handler.tags = ['search'];
-
-export default handler;
+handler = ['tiktokvid', 'tts', 'ttv']
