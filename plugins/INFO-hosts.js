136b919b974d4249d65a6030fbfc18ec8ef1c516
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-26 12:11:40 -0500

    Actualizar INFO-hosts.js

--- plugins/INFO-hosts.js
+++ plugins/INFO-hosts.js
@@ -26,7 +26,7 @@ body: `✅ HOSTIN DE CALIDAD ✅`,
 "previewType": "PHOTO",
 thumbnailUrl: 'https://qu.ax/EQTd.jpg', 
 sourceUrl: 'https://dashboard.infinitywa.xyz'}}},
-{ quoted: estilo})
+{ quoted: fkontak})
 }
 handler.tags =['info'] 
 handler.help = ['infinity', 'host', 'hosting'] 
