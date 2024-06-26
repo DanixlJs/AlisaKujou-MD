878352578c80752cc62051a1200b1e39b337ecde
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-26 19:07:45 +0200

    Actualizar INFO-hosts.js

--- plugins/INFO-hosts.js
+++ plugins/INFO-hosts.js
@@ -1,5 +1,30 @@
-let handler = async (m, { conn, command, usedPrefix }) => {
-let txt = `❀ *INFINITY HOST* ❀
+const handler = async (m, { conn, command }) => {
+    if (command === 'infinity') {
+        let infifake = {
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 1,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363160031023229@newsletter',
+                    serverMessageId: '',
+                    newsletterName: '♾️ INFINITY - HOST ♾️'
+                },
+                externalAdReply: {
+                    title: '✅ HOSTIN DE CALIDAD ✅',
+                    body: '❀ Y buen precio 🌟',
+                    mediaType: 1,
+                    renderLargerThumbnail: false,
+                    previewType: 'PHOTO',
+                    thumbnailUrl: 'https://telegra.ph/file/5a177278036dd3966b6b2.jpg',
+                    thumbnail: 'https://telegra.ph/file/5a177278036dd3966b6b2.jpg',
+                    sourceUrl: 'https://dashboard.infinitywa.xyz'
+                }
+            },
+            quoted: m
+        };
+
+        let txt = `❀ *INFINITY HOST* ❀
 *¿Quieres un hosting para poner tu Bot y no sabes cual usar?*
 Pues te presento a *Infinity Host*, un hosting que te ofrece una gran variedad de servicios para poner tus proyectos y que estos estén 24/7 Activos.
 
@@ -13,10 +38,13 @@ Pues te presento a *Infinity Host*, un hosting que te ofrece una gran variedad d
 ✰ *Página:* https://dashboard.infinitywa.xyz
 ✰ *Panel:* https://store.panel-infinitywa.store
 
-*¿Qué esperas para Adquirir sus servicios? Únete a esta familia :)*`
-conn.reply(m.chat, txt, m, fake, )
-}
-handler.tags =['info'] 
-handler.help = ['infinity', 'host', 'hosting'] 
-handler.command = /^(infinity|infinityWa|host|hosting)$/i
-export default handler
+*¿Qué esperas para Adquirir sus servicios? Únete a esta familia :)*
+`;
+
+        await conn.reply(m.chat, txt, m, infifake, );
+    }
+};
+
+handler.command = ['infinity'];
+
+export default handler;
