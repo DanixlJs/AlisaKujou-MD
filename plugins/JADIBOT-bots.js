38a0e6f51e13adf5c10dbaf535c7bf51fe5ad198
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-29 04:42:38 +0200

    Actualizar JADIBOT-bots.js

--- plugins/JADIBOT-bots.js
+++ plugins/JADIBOT-bots.js
@@ -11,7 +11,9 @@ async function handler(m, { conn: stars, usedPrefix }) {
   let replyMessage = message.length === 0 ? '' : message
   global.totalUsers = users.length
   let responseMessage = `╭──「${global.wm}」╼\n│❀ *Total Sub-Bots ≫* ${totalUsers || '0'}\n╰────╼\n\n${replyMessage.trim()}`.trim()
-  await stars.reply(m.chat, responseMessage, m, { mentions: await stars.parseMention(responseMessage)} )
+  await stars.seendMessage(m.chat, { image: { url: "https://telegra.ph/file/b867188ad3e6ea1f593ad.png" }, caption: responseMessage, mentions: stars.parseMentions(responseMessage), quoted: m})
+
+  //await stars.reply(m.chat, responseMessage, m, { mentions: await stars.parseMention(responseMessage)} )
 }
 
 handler.command = ['bots']
