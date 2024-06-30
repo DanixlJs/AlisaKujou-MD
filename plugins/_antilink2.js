42054872b59b0a4f5403c5ecd0e28cd26ab30d61
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-30 17:45:30 +0200

    Actualizar _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -1,25 +1,32 @@
-const linkRegex = /https:/i;
-export async function before(m, {isAdmin, isBotAdmin, text}) {
-  if (m.isBaileys && m.fromMe) {
-    return !0;
-  }
-  if (!m.isGroup) return !1;
-  const chat = global.db.data.chats[m.chat];
-  const delet = m.key.participant;
-  const bang = m.key.id;
-  const bot = global.db.data.settings[this.user.jid] || {};
-  if (bot) return;
-  const user = `@${m.sender.replace('@s.whatsapp.net', '').trim()}`;
-  const isGroupLink = linkRegex.exec(m.text);
-  if (chat.antiLink2 && isGroupLink && !isAdmin) {
-    if (isBotAdmin) {
-      const l1 = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
-      if (m.text.includes(l1)) return !0;
+let linkRegex = /\b((https?:\/\/|www\.)?[\w-]+\.[\w-]+(?:\.[\w-]+)*(\/[\w\.\-\/]*)?)\b/i;
+
+export async function before(m, { isAdmin, isBotAdmin, text }) {
+    if (m.isBaileys && m.fromMe) {
+        return true;
     }
-	if (!isBotAdmin) return; 
-    await this.sendMessage(m.chat, {text: `✧ 「 ${user} 」envió un Link que contiene *https://*, procedo a eliminarlo.`, mentions: [m.sender]}, {quoted: m});
-    if (isBotAdmin) {
-      await this.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
-      const responseb = await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      if (responseb[0].status === '404') return;
-    }}}
+    if (!m.isGroup) return false;
+
+    const chat = global.db.data.chats[m.chat];
+    const delet = m.key.participant;
+    const bang = m.key.id;
+    const bot = global.db.data.settings[this.user.jid] || {};
+    const user = `@${m.sender.split`@`[0]}`;
+    const isGroupLink = linkRegex.exec(m.text);
+
+    if (chat.antiLink2 && isGroupLink && !isAdmin) {
+        if (isBotAdmin) {
+            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
+            if (m.text.includes(linkThisGroup)) return;
+        }
+        
+        if (isAdmin) return;
+        if (isBotAdmin) return;
+
+        await this.sendMessage(m.chat, { text: `✧ Se detectó un Link qué contiene *https*, procedo a eliminarlo.`, mentions: [m.sender] }, { quoted: m });
+        
+        if (isBotAdmin) {
+            await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
+        }
+    }
+    return;
+}
