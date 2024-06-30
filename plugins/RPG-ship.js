e4c5191ce86d5d5da0c38b98651606964441fa48
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-30 17:28:54 +0200

    Actualizar RPG-ship.js

--- plugins/RPG-ship.js
+++ plugins/RPG-ship.js
@@ -1,17 +1,14 @@
 var handler = async (m, { conn, command, text }) => {
 if (!global.db.data.chats[m.chat].rpg) return m.reply('✧ Los comandos de RPG han sido desactivados en este grupo.')
-if (!text) return conn.reply(m.chat, `✧ _Escribe el nombre de dos personas para calcular su amor_`, m, fake, )
+if (!text) return conn.reply(m.chat, `✧ Escribe el nombre de dos personas para calcular su amor`, m,)
 let [text1, ...text2] = text.split(' ')
-
 text2 = (text2 || []).join(' ')
-if (!text2) return conn.reply(m.chat, `✧ _Escribe el nombre de la segunda persona_`, m, fake, )
-let love = `❤️ *${text1}* tu oportunidad de enamorarte de *${text2}* es de ${Math.floor(Math.random() * 100)}% 👩🏻‍❤️‍👨🏻`
-
+if (!text2) return conn.reply(m.chat, `✧ Escribe el nombre de la segunda persona.`, m,)
+let love = `❀ *${text1}* tu oportunidad de enamorarte de *${text2}* es del ${Math.floor(Math.random() * 100)}% 👩🏻‍❤️‍👨🏻`
 m.reply(love, null, { mentions: conn.parseMention(love) })
-
 }
-handler.help = ['ship']
+handler.help = ['ship <@tag1> <@tag2>']
 handler.tags = ['rpg']
-handler.command = /^(ship)$/i
-
+handler.command = ['ship']
+handler.registrado = true
 export default handler
