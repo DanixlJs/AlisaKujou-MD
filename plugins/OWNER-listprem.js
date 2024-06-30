d8b4a1debeb47bb9cdb2e5517625db0be503770d
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-30 19:01:37 +0200

    Actualizar plugins

---
+++ plugins/OWNER-listprem.js
@@ -0,0 +1,17 @@
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+    let users = global.db.data.users;
+    let premiumUsers = Object.keys(users).filter(user => users[user].premium);
+    if (premiumUsers.length === 0) {
+        return m.reply('✧ No hay usuarios premium.');
+    }
+    let message = '❀ *USUARIOS PREMIUMS*';
+    for (let user of premiumUsers) {
+        message += `> → @${user.split('@')[0]}\n> → `;
+    }
+    await conn.reply(m.chat, message, m, { mentions: conn.parseMention(message)});
+};
+handler.command = ["listprem"];
+handler.help = ["listprem"];
+handler.rowner = true;
+handler.registrado = true;
+export default handler;
