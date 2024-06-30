825c6c63f57e8e7311ed12f37feb665d62ff955d
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-06-30 17:39:35 +0200

    Actualizar _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,31 +1,32 @@
-const linkRegex = /chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i;
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
 
-export async function before(m, { conn, isAdmin, isBotAdmin }) {
- if (m.isBaileys && m.fromMe) {
-  return !0;
- }
- if (!m.isGroup) return !1;
+export async function before(m, { isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe) return true;
+    if (!m.isGroup) return false;
 
- let chat = global.db.data.chats[m.chat];
- let bot = global.db.data.settings[this.user.jid] || {};
+    let chat = global.db.data.chats[m.chat];
+    let delet = m.key.participant;
+    let bang = m.key.id;
+    let bot = global.db.data.settings[this.user.jid] || {};
+    const isGroupLink = linkRegex.exec(m.text);
+    const grupo = `https://chat.whatsapp.com`;
 
- if (!chat || bot) return;
+    if (isAdmin && chat.antiLink && m.text.includes(grupo)) return;
+    if (chat.antiLink && isGroupLink && !isAdmin) {
+        if (isBotAdmin) {
+            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
+            if (m.text.includes(linkThisGroup)) return;
+        }
 
- const isGroupLink = linkRegex.exec(m.text);
- if (chat.antiLink && isGroupLink && !isAdmin) {
-	if (!isBotAdmin) return;
-  if (isBotAdmin) {
-   const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
-  if (m.text.includes(linkThisGroup)) return !0;
-  }
-  await conn.reply(m.chat, `❀ 『${m.sender.replace('@s.whatsapp.net', '').trim()}』 será eliminado del grupo por enviar un Link de WhatsApp.`, null, { mentions: [m.sender] });
+        if (!isBotAdmin) return;
 
-  if (isBotAdmin && chat.antiLink) {
-   await conn.sendMessage(m.chat, { delete: m.key });
-   await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-  } else if (!chat.antiLink) {
-   return;
-  }
- }
- return !0;
+        await conn.reply(m.chat, `✧ ${m.sender.replace('@s.whatsapp.net', '').trim()} será eliminado del grupo por enviar un Link de WhatsApp.`, m);
+
+        if (isBotAdmin) {
+            await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
+            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+        }
+    }
+
+    return;
 }
