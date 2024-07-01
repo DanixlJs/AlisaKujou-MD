3e83358d1d59e9d2e5d772ec255582f90ac2851d
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-07-02 00:42:08 +0200

    Actualizar DOWNLOADER-apk.js

--- plugins/DOWNLOADER-apk.js
+++ plugins/DOWNLOADER-apk.js
@@ -7,11 +7,12 @@ if (size.includes('GB') || size.replace(' MB', '') > 200) { return await m.reply
 let txt = `❀ *APTOIDE DL*\n✰ *Nombre ⪼* ${name}\n◈ *Packname ⪼* ${packname}\n◈ *Peso ⪼* ${size}\n◈ *Actualizado el ⪼* ${update}`
 await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m)
 await conn.sendMessage(m.chat, {document: { url: dl_url }, mimetype: 'application/vnd.android.package-archive', fileName: name + '.apk', caption: null }, {quoted: m})
-} catch {
+} catch (e){
+m.reply('✧ Ocurrió un error inesperado.')
+console.log(e)
 }}
 handler.help = ['apk <aplicación>']
 handler.tags = ['downloader']
 handler.command = ['aptoide', 'apk']
 handler.registrado = true 
-handler.diamantes = 1
 export default handler
